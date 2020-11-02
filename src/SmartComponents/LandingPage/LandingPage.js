a77b2485265b5e5c98fe625c8df38949cec53d03
Jenn Giardino <jgiardin@redhat.com>
2020-11-02 10:13:35 -0500

    Rearranged landing page to have a table and action
    As mentioned in the previous commit, `<Main>` doesn't really
    belong in App.js, it instead belongs in LandingPage.js, because
    it creates the html/css wrapper for the main contents, as a
    sibling to the `<PageHeader>` component used in LandingPage.js.
    However, including `<Main>` in LandingPage.js will cause the
    tests to fail. Therefore, just the html and classes that
    `<Main>` creates is used in LandingPage.js.

--- src/SmartComponents/LandingPage/LandingPage.js
+++ src/SmartComponents/LandingPage/LandingPage.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { Flex, FlexItem, FlexModifiers } from '@patternfly/react-core';
-import {
-    PageHeader,
-    PageHeaderTitle
-} from '@redhat-cloud-services/frontend-components';
+import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 
-import CreateImageCard from './CreateImageCard';
-import ImagesCard from './ImagesCard.js';
+import ImagesTable from '../ImagesTable/ImagesTable';
 
-/**
- * A smart component that handles all the api calls and data needed by the dumb components.
- * Smart components are usually classes.
- *
- * https://reactjs.org/docs/components-and-props.html
- * https://medium.com/@thejasonfile/dumb-components-and-smart-components-e7b33a698d43
- */
 class LandingPage extends Component {
     constructor(props) {
         super(props);
@@ -26,16 +14,11 @@ class LandingPage extends Component {
         return (
             <React.Fragment>
                 <PageHeader>
-                    <PageHeaderTitle title='Image Builder' />
+                    <PageHeaderTitle title='Images' />
                 </PageHeader>
-                <Flex>
-                    <FlexItem breakpointMods={ [{ modifier: FlexModifiers.column }, { modifier: FlexModifiers['flex-1'] }] }>
-                        <CreateImageCard />
-                    </FlexItem>
-                    <FlexItem breakpointMods={ [{ modifier: FlexModifiers.column }, { modifier: FlexModifiers['flex-2'] }] }>
-                        <ImagesCard />
-                    </FlexItem>
-                </Flex>
+                <section className="pf-l-page__main-section pf-c-page__main-section">
+                    <ImagesTable />
+                </section>
             </React.Fragment>
         );
     }
