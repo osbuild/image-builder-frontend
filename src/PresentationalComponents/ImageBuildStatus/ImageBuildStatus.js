c73df5b1d8e922e482ec0de0ee508d2857a45365
Jenn Giardino <jgiardin@redhat.com>
2020-11-06 08:46:45 -0500

    Adds icons and text strings for image build status

---
+++ src/PresentationalComponents/ImageBuildStatus/ImageBuildStatus.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Flex, Spinner } from '@patternfly/react-core';
+import { CheckCircleIcon, PendingIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
+
+import './ImageBuildStatus.scss';
+
+const ImageBuildStatus = (props) => {
+    const messages = {
+        success: [
+            {
+                icon: <CheckCircleIcon className="success" />,
+                text: 'Ready'
+            }
+        ],
+        failure: [
+            {
+                icon: <ExclamationCircleIcon className="error" />,
+                text: 'Image build failed'
+            }
+        ],
+        pending: [
+            {
+                icon: <PendingIcon />,
+                text: 'Image build, Upload, Cloud registration pending'
+            }
+        ],
+        building: [
+            {
+                icon: <Spinner size="md" />,
+                text: 'Image build in progress'
+            },
+            {
+                icon: <PendingIcon />,
+                text: 'Upload, Cloud registration pending'
+            }
+        ],
+        uploading: [
+            {
+                icon: <Spinner size="md" />,
+                text: 'Upload in progress'
+            },
+            {
+                icon: <PendingIcon />,
+                text: 'Cloud registration pending'
+            }
+        ],
+        registering: [
+            {
+                icon: <Spinner size="md" />,
+                text: 'Cloud registration in progress'
+            }
+        ]
+    };
+    return (
+        <React.Fragment>
+            {messages[props.status] &&
+                messages[props.status].map((message, key) => (
+                    <Flex key={ key } className="pf-u-align-items-baseline pf-m-nowrap">
+                        <div>{message.icon}</div>
+                        <small>{message.text}</small>
+                    </Flex>
+                ))
+            }
+        </React.Fragment>
+    );
+};
+
+ImageBuildStatus.propTypes = {
+    status: PropTypes.string,
+};
+
+export default ImageBuildStatus;
