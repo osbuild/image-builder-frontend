a9eaef724886a4ff2504a9e888b691d2ff560e89
Aleksandar Todorov <atodorov@redhat.com>
2020-11-30 15:39:57 +0200

    Experiment with cypress & cypress-react-select

---
+++ cypress/integration/CreateImageWizard.spec.js
@@ -0,0 +1,15 @@
+describe('e2e test with cypress', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true);
+    });
+
+//    beforeEach(() => {
+//        cy.visit('/image-builder/landing');
+//        cy.waitForReact();
+//    });
+
+//    it('findByLabelText', () => {
+//        cy.findByTestId('create-image-action');
+//        cy.findByTestId('images-table');
+//    });
+});
