158392839f35ab6e2fd7902be5b69b23ab6efc01
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-02-05 12:45:52 +0100

    travis: Let main push to prod-beta as well

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,7 +4,7 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "main" ]
 then
-    for env in ci qa stage
+    for env in ci qa stage prod
     do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
@@ -23,8 +23,9 @@ then
     done
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
-    echo "PUSHING ${TRAVIS_BRANCH}"
-    rm -rf ./build/.git
-    .travis/release.sh "${TRAVIS_BRANCH}"
-fi
+# Enable this once we fork prod from stage
+# if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+#     echo "PUSHING ${TRAVIS_BRANCH}"
+#     rm -rf ./build/.git
+#     .travis/release.sh "${TRAVIS_BRANCH}"
+# fi
