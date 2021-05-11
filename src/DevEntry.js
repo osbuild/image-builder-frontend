7eee835a6b5a21c27e3101d0db205c467bc66186
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-05-11 14:54:22 +0200

    src: Have separate AppEntry points for dev and prod
    The production AppEntry shouldn't pass any middlewares to the store.

--- src/DevEntry.js
+++ src/DevEntry.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
@@ -6,12 +7,16 @@ import App from './App';
 
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
 
-const ImageBuilder = () => (
-    <Provider store={ init({}).getStore() }>
+const ImageBuilder = ({ logger }) => (
+    <Provider store={ init({}, logger).getStore() }>
         <Router basename={ getBaseName(window.location.pathname) }>
             <App />
         </Router>
     </Provider>
 );
+
+ImageBuilder.propTypes = {
+    logger: PropTypes.any
+};
 
 export default ImageBuilder;
