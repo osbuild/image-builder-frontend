c7e8fc57ec197d613e7eab8bb8f297a3116c48c1
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-05-19 16:40:11 +0200

    src: Update documentation links

--- src/Components/CreateImageWizard/CreateImageWizard.js
+++ src/Components/CreateImageWizard/CreateImageWizard.js
@@ -305,7 +305,9 @@ class CreateImageWizard extends Component {
                             icon={ <ExternalLinkAltIcon /> }
                             iconPosition="right"
                             isInline
-                            href="https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8-beta/">
+                            href="
+https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/uploading_a_customized_rhel_system_image_to_cloud_environments/index
+                            ">
                                 Documentation
                         </Button>
                     </> }
