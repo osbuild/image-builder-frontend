fc22ba69e0afd998e6cb094d3b431c4dba1063a5
Jacob Kozol <jacobdkozol@gmail.com>
2021-05-20 15:01:35 +0200

    CreateImageWizard: fix OAuth info link style

--- src/Components/CreateImageWizard/WizardStepUploadAzure.js
+++ src/Components/CreateImageWizard/WizardStepUploadAzure.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actions } from '../../store/actions';
 
-import { Form, FormGroup, Text, TextContent, TextInput, Title } from '@patternfly/react-core';
+import { Button, Form, FormGroup, Text, TextContent, TextInput, Title } from '@patternfly/react-core';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 import './WizardStepUploadAzure.scss';
@@ -26,17 +26,25 @@ class WizardStepUploadAzure extends Component {
             To authorize Image Builder to push images to Microsoft Azure, the account owner
             must configure Image Builder as an authorized application and give it the role of
             &quot;Contributor&quot; to at least one resource group.<br />
-                        <a href="https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow">
-                            <small>Learn more about OAuth 2.0</small></a>
                     </Text>
-
+                    <small>
+                        <Button
+                            component="a"
+                            target="_blank"
+                            variant="link"
+                            icon={ <ExternalLinkAltIcon /> }
+                            iconPosition="right"
+                            isInline
+                            href="https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow">
+                            Learn more about OAuth 2.0
+                        </Button>
+                    </small>
                     <a href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=b94bb246-b02c-4985-9c22-d44e66f657f4
 &scope=openid&response_type=code&response_mode=form_post
 &redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fcommon%2Foauth2%2Fnativeclient" target="_blank" rel="noopener noreferrer">
             Authorize Image Builder on Azure <ExternalLinkAltIcon />
                     </a>
                 </TextContent>
-
                 <Title headingLevel="h3">Destination</Title>
                 <Text>
             Your image will be uploaded to the resource group in the subscription you specify.
