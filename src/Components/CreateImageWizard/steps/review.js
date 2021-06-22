dc5a198df7b7eed23a8557e15804ef9b71691243
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-06-22 14:38:07 +0200

    CreateImageWizard: Remove unecessary lint rule disable

--- src/Components/CreateImageWizard/steps/review.js
+++ src/Components/CreateImageWizard/steps/review.js
@@ -3,7 +3,6 @@ import CustomButtons from '../formComponents/CustomSubmitButtons';
 export default {
     name: 'review',
     title: 'Review',
-    // eslint-disable-next-line react/display-name
     buttons: CustomButtons,
     fields: [
         {
