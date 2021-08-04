fa7746b694ce987e2fbc10b182e6f4706d298278
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-08-04 09:16:11 +0200

    store: Pass middleware correctly
    Get rid of duplicate entry points.
    
    Depending on the NODE_ENV, add the logger middleware in AppEntry.
    
    Filter out undefined items to avoid 'middleware is not a function' error.

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
-
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
+import logger from 'redux-logger';
 
 const ImageBuilder = () => (
-    <Provider store={ init({}).getStore() }>
+    <Provider store={ init({}, process.env.NODE_ENV !== 'production' ? logger : undefined).getStore() }>
         <Router basename={ getBaseName(window.location.pathname) }>
             <App />
         </Router>
