fb8f19cd2140adbe2af58dc01390144d5b919987
Jacob Kozol <jacobdkozol@gmail.com>
2022-01-12 18:37:17 +0100

    ImagesTable: add target labels for new image types
    The "guest-image", "image-installer", and "vsphere" image types now
    display proper labels in the ImagesTable target column. Also, the upload
    component is renamed to Target for consistency.

---
+++ src/Components/ImagesTable/Target.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Target = (props) => {
+    const targetOptions = {
+        aws: 'Amazon Web Services',
+        azure: 'Microsoft Azure',
+        gcp: 'Google Cloud Platform',
+        vsphere: 'VMWare',
+        'guest-image': 'Virtualization - Guest image',
+        'image-installer': 'Bare metal - Installer'
+    };
+
+    let target;
+    if (props.uploadType === 'aws.s3') {
+        target = targetOptions[props.imageType];
+    } else {
+        target = targetOptions[props.uploadType];
+    }
+
+    return (
+        <>
+            {target}
+        </>
+    );
+};
+
+Target.propTypes = {
+    uploadType: PropTypes.string,
+    imageType: PropTypes.string
+};
+
+export default Target;
