b4fb26e749bc83ae8bbbf8e8531105db60f88dd2
lucasgarfield <lucas@redhat.com>
2022-03-04 10:44:22 +0100

    CreateImageWizard: Use new DocumentationButton component in LandingPage

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,10 +5,11 @@ import React, { Component } from 'react';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 
 import { Button, Popover, TextContent, Text } from '@patternfly/react-core';
-import { ExternalLinkAltIcon, GithubIcon, HelpIcon } from '@patternfly/react-icons';
+import { GithubIcon, HelpIcon } from '@patternfly/react-icons';
 
 import ImagesTable from '../ImagesTable/ImagesTable';
 import './LandingPage.scss';
+import DocumentationButton from '../sharedComponents/DocumentationButton';
 
 class LandingPage extends Component {
     constructor(props) {
@@ -27,18 +28,7 @@ class LandingPage extends Component {
                                         Image Builder is a service that allows you to create RHEL images
                                         and push them to cloud environments.
                             </Text>
-                            <Button
-                                component="a"
-                                target="_blank"
-                                variant="link"
-                                icon={ <ExternalLinkAltIcon /> }
-                                iconPosition="right"
-                                isInline
-                                href="
-https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/uploading_a_customized_rhel_system_image_to_cloud_environments/index
-                                ">
-                                    Documentation
-                            </Button>
+                            <DocumentationButton />
                             <br />
                             <Button
                                 component="a"
