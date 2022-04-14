osbuild/image-builder-frontend

commit dd6f91c59932e12eac2db973162b271017bbb441

config: Prep prod webpack config for ga

config/prod.webpack.config.js
@@ -6,7 +6,7 @@ const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
     modules: [ 'image_builder' ],
     sassPrefix: '.imageBuilder, .image_builder',
-    ...(process.env.BETA && { deployment: 'beta/apps' }),
+    deployment: process.env.BETA ? 'beta/apps' : 'apps',
 });
 
 plugins.push(

commit 79e865750eed174a5bea96362fde292ebd4acc87

config: Let frontend-components set deployment in prod config
This got broken in #696, `deployment` never actually got set during the
release build process, and now we default to
`apps`. `frontend-components` will automatically do the right thing and
set the deployment based on the branch name.

config/prod.webpack.config.js
@@ -6,7 +6,6 @@ const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
     modules: [ 'image_builder' ],
     sassPrefix: '.imageBuilder, .image_builder',
-    deployment: process.env.BETA ? 'beta/apps' : 'apps',
 });
 
 plugins.push(