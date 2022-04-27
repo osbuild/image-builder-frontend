db4b17ed76586610d9049782d7eaeca960ce47c7
Gianluca Zuccarelli <gzuccare@redhat.com>
2022-04-27 17:00:45 +0100

    ImagesTable: add error status
    Add error information for failed image
    builds. The error details are only displayed
    for failed builds and the error highest up
    the error chain is displayed in the expandable
    section of the image.

---
+++ src/Components/ImagesTable/ImageBuildErrorDetails.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from '@patternfly/react-core';
+
+const useGetErrorReason = (err) => {
+    if (!err?.reason) {
+        return 'An unknown error occured';
+    }
+
+    if (err.details?.reason) {
+        return err.details.reason;
+    }
+
+    return err.reason;
+};
+
+const ErrorDetails = ({ status }) => {
+    if (!status || status.status !== 'failure') {
+        return <></>;
+    }
+
+    const reason = useGetErrorReason(status.error);
+
+    return (
+        <div className='pf-u-mt-sm'>
+            <strong>Status</strong>
+            <Alert variant="danger" title="Image build failed" isInline isPlain />
+            <p className='pf-u-danger-color-200 pf-u-w-33-on-md'>{reason}</p>
+        </div>
+    );
+};
+
+ErrorDetails.propTypes = {
+    status: PropTypes.object,
+};
+
+export default ErrorDetails;
