070e507933fe27ac3471d960ff12937c410b767a
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2022-05-12 11:13:53 +0200

    constants: Switch to rhel-86

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const IMAGE_BUILDER_API = '/api/image-builder/v1';
 export const RHSM_API = '/api/rhsm/v2';
-export const RHEL_8 = 'rhel-85';
+export const RHEL_8 = 'rhel-86';
 
 export const UNIT_KIB = 1024 ** 1;
 export const UNIT_MIB = 1024 ** 2;
