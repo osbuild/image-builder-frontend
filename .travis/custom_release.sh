d33544eb240b89fd8a6dfa4f62b90a0b1cc7e09d
Jakub Rusz <jrusz@redhat.com>
2022-06-03 12:32:52 +0200

    CI: Fix location of Jenkinsfile for sed
    The Jenkinsfile is actually downloaded to .travis folder so fix the
    location for the sed command to work.

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -9,7 +9,7 @@ if [ "${TRAVIS_BRANCH}" = "stage-stable" ]; then
     # Download modified Jenkinsfile
     curl -o .travis/58231b16fdee45a03a4ee3cf94a9f2c3 https://raw.githubusercontent.com/RedHatInsights/image-builder-frontend/stage-stable/.travis/Jenkinsfile
     # Insert stage webhook URL
-    sed -i 's|WEBHOOK_PLACEHOLDER|https://smee.io/IQDT9yRXsWlqbxpg|g' 58231b16fdee45a03a4ee3cf94a9f2c3
+    sed -i 's|WEBHOOK_PLACEHOLDER|https://smee.io/IQDT9yRXsWlqbxpg|g' .travis/58231b16fdee45a03a4ee3cf94a9f2c3
     .travis/release.sh "stage-stable"
 fi
 
@@ -21,6 +21,6 @@ if [ "${TRAVIS_BRANCH}" = "prod-stable" ]; then
     # Download modified Jenkinsfile
     curl -o .travis/58231b16fdee45a03a4ee3cf94a9f2c3 https://raw.githubusercontent.com/RedHatInsights/image-builder-frontend/stage-stable/.travis/Jenkinsfile
     # Insert prod webhook URL
-    sed -i 's|WEBHOOK_PLACEHOLDER|https://smee.io/F9gZwIGELxwah4if|g' 58231b16fdee45a03a4ee3cf94a9f2c3
+    sed -i 's|WEBHOOK_PLACEHOLDER|https://smee.io/F9gZwIGELxwah4if|g' .travis/58231b16fdee45a03a4ee3cf94a9f2c3
     .travis/release.sh "prod-stable"
 fi
