7eb202e800efdce490d527116dcb52dae8791ad9
Gianluca Zuccarelli <gzuccare@redhat.com>
2022-07-14 12:14:07 +0100

    devel: remove frontend spandx proxy
    Since the frontend uses its own webpack
    proxy, these config options have been removed
    from the spandx config and only the backend
    proxy is being configured.

--- devel/config/spandx/local-frontend-and-api.js
+++ devel/config/spandx/local-frontend-and-api.js
@@ -1,15 +1,11 @@
 /*global module*/
 
-const SECTION = 'insights';
-const APP_ID = 'image-builder';
-const FRONTEND_PORT = 8002;
 const API_PORT = 8086;
-const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://frontend:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://frontend:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://frontend:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://frontend:${FRONTEND_PORT}` };
-routes[`/api/${APP_ID}`]             = { host: `http://backend:${API_PORT}` };
+const routes = {
+  "/api/image-builder": { 
+    host: `http://backend:${API_PORT}` 
+  }
+};
 
 module.exports = { routes };
