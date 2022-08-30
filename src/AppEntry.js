64ce13767eb834b433a14e883fa080b453e09076
lucasgarfield <lucas@redhat.com>
2022-08-30 17:42:50 +0200

    Redux: Add Redux Toolkit
    This commit adds Redux Toolkit as a dependency. Adding Redux Toolkit is
    beneficial because it will allow us to use the current best-practice Redux
    development patterns and tools as well as reduce the number of
    dependencies. Redux Toolkit is backwards compatible with existing Redux code,
    and therefore works with all of the current actions and reducers.
    
    The store is now created using Redux Toolkit's configureStore(). Previously, a
    custom store creator developed by Red Hat Insights was used. However,
    this is actually not required for Insights apps, and creating the store using
    configureStore() is necessary to take advantage of Redux Toolkit. For
    instance, the store can now be inspected using the Redux devtools in the
    browser.
    
    This commit removes the redux-logger middleware. It is no longer
    necessary, as the Redux development tools can now be used to easily inspect and
    reason about the redux store and its state.
    
    The Thunk middleware dependency has also been removed, as Thunks are already
    included in Redux Toolkit.
    
    The redux-promise-middleware dependency has been left in place for now,
    but its functionality is also available in Redux Toolkit and it may be
    considered for removal in the future.
    
    Using Redux Toolkit will also allow us to move to the `Slice` pattern when
    defining actions/reducers in the future if we wish. This will make
    writing, reasoning about, and debugging the code related to the Redux
    store much easier.

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { init } from './store';
 import App from './App';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
-import logger from 'redux-logger';
+import { store } from './store';
 
 const ImageBuilder = () => (
-  <Provider
-    store={init(
-      {},
-      ...[process.env.NODE_ENV !== 'production' ? logger : undefined]
-    ).getStore()}
-  >
+  <Provider store={store}>
     <Router basename={getBaseName(window.location.pathname)}>
       <App />
     </Router>
--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,14 @@
-import { ReducerRegistry } from '@redhat-cloud-services/frontend-components-utilities/ReducerRegistry';
+import { configureStore } from '@reduxjs/toolkit';
+import composes from './reducers/composes';
 import promiseMiddleware from 'redux-promise-middleware';
-import thunk from 'redux-thunk';
 import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
 
-import composes from './reducers/composes';
+export const reducer = {
+  composes: composes,
+  notifications: notificationsReducer,
+};
 
-let registry;
+export const middleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(promiseMiddleware);
 
-export function init(store = {}, ...middleware) {
-  if (!registry) {
-    registry = new ReducerRegistry(store, [
-      promiseMiddleware,
-      thunk,
-      ...middleware.filter((item) => typeof item !== 'undefined'),
-    ]);
-
-    registry.register({
-      composes,
-      notifications: notificationsReducer,
-    });
-  }
-
-  return registry;
-}
-
-export function getStore() {
-  return registry.getStore();
-}
-
-export function register(...args) {
-  return registry.register(...args);
-}
-
-/* added for testing purposes only */
-export function clearStore() {
-  registry = undefined;
-}
+export const store = configureStore({ reducer, middleware });
