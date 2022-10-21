1beffe773e15c3f36f6739058e4e5206c2305297
lucasgarfield <lucas@redhat.com>
2022-10-21 16:10:04 +0200

    Constants: Add constants for AWS regions

--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,32 @@ export const RELEASES = {
   'centos-8': 'CentOS Stream 8',
   'centos-9': 'CentOS Stream 9',
 };
+
+export const DEFAULT_AWS_REGION = 'us-east-1';
+
+// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html
+export const AWS_REGIONS = [
+  { description: 'US East (Ohio)', value: 'us-east-2' },
+  { description: 'US East (N. Virginia)', value: 'us-east-1' },
+  { description: 'US West (N. California)', value: 'us-west-1' },
+  { description: 'US West (Oregon)', value: 'us-west-2' },
+  { description: 'Africa (Cape Town)', value: 'af-south-1' },
+  { description: 'Asia Pacific (Hong Kong)', value: 'ap-east-1' },
+  { description: 'Asia Pacific (Jakarta)', value: 'ap-southeast-3' },
+  { description: 'Asia Pacific (Mumbai)', value: 'ap-south-1' },
+  { description: 'Asia Pacific (Osaka)', value: 'ap-northeast-3' },
+  { description: 'Asia Pacific (Seoul)', value: 'ap-northeast-2' },
+  { description: 'Asia Pacific (Singapore)', value: 'ap-southeast-1' },
+  { description: 'Asia Pacific (Sydney)', value: 'ap-southeast-2' },
+  { description: 'Asia Pacific (Tokyo)', value: 'ap-northeast-1' },
+  { description: 'Canada (Central)', value: 'ca-central-1' },
+  { description: 'Europe (Frankfurt)', value: 'eu-central-1' },
+  { description: 'Europe (Ireland)', value: 'eu-west-1' },
+  { description: 'Europe (London)', value: 'eu-west-2' },
+  { description: 'Europe (Milan)', value: 'eu-south-1' },
+  { description: 'Europe (Paris)', value: 'eu-west-3' },
+  { description: 'Europe (Stockholm)', value: 'eu-north-1' },
+  { description: 'Middle East (Bahrain)', value: 'me-south-1' },
+  { description: 'Middle East (UAE)', value: 'me-central-1' },
+  { description: 'South America (S\u00e3o Paolo)', value: 'sa-east-1' },
+];
