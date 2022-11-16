6977388755b9316175c6ca43c151ac53baa88a4b
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2022-11-16 14:13:16 +0100

    DocumentationButton: fix documentation url
    The old URL pointed to the on-prem documentation, not the service docs.

--- src/Components/sharedComponents/DocumentationButton.js
+++ src/Components/sharedComponents/DocumentationButton.js
@@ -4,7 +4,7 @@ import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 const DocumentationButton = () => {
   const documentationURL =
-    'https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html/composing_a_customized_rhel_system_image/index';
+    'https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/creating_customized_rhel_images_using_the_image_builder_service/index';
 
   return (
     <Button
