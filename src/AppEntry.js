2256d1cd7901beb1420a8967978c99b5ff0e6e39
Martin Maroši <marvusm.mmi@gmail.com>
2022-11-28 11:31:13 +0100

    Change routing to consume chrome history context.

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import { store } from './store';
-import { getBaseName } from './Utilities/path';
 
 const ImageBuilder = () => (
   <Provider store={store}>
-    <Router basename={getBaseName(window.location.pathname)}>
-      <App />
-    </Router>
+    <App />
   </Provider>
 );
 
