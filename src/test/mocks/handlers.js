6d2dbb60e927af6b7f56f15a9be7b3ebd70fd363
lucasgarfield <lucas@redhat.com>
2023-02-06 15:07:45 +0100

    MSW: Add mock definition for /provisioning/v1/sources end point
    This commit adds the necessarily handler, resolver, and server to use
    MSW for mocking the /provisioning/v1/sources endpoint.
    
    The server can be used in tests like so:
    ```javascript
    // src/setupTests.js
    import { server } from './mocks/server.js'
    // Establish API mocking before all tests.
    beforeAll(() => server.listen())
    
    // Reset any request handlers that we may add during the tests,
    // so they don't affect other tests.
    afterEach(() => server.resetHandlers())
    
    // Clean up after the tests are finished.
    afterAll(() => server.close())
    ```

---
+++ src/test/mocks/handlers.js
@@ -0,0 +1,20 @@
+import { rest } from 'msw';
+
+import { PROVISIONING_SOURCES_ENDPOINT } from '../../constants';
+
+// TODO add this endpoint to constants
+export const handlers = [
+  rest.get(PROVISIONING_SOURCES_ENDPOINT, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: '123',
+          name: 'my_source',
+          source_type_id: '1',
+          uid: 'de5e35d4-4c1f-49d7-9ef3-7d08e6b9c76a',
+        },
+      ])
+    );
+  }),
+];
