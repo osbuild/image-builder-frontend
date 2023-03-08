8a3bdde3464c9c902618651b96c411b2faac5a4b
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-03-08 10:34:09 +0100

    CreateImageWizard: update register later text

--- src/Components/CreateImageWizard/steps/registration.js
+++ src/Components/CreateImageWizard/steps/registration.js
@@ -127,9 +127,24 @@ export default {
           <Text component={TextVariants.h3}>Register Later</Text>
           <Text>
             On initial boot, systems will need to be registered manually before
-            having access to updates or Red Hat services.
+            having access to updates or Red Hat services. Registering and
+            connecting your systems during the image creation is recommended.
           </Text>
-          <Text>Registering now is recommended.</Text>
+          <Text>
+            If you prefer to register later, review the instructions for manual
+            registration with remote host configuration.
+          </Text>
+          <Button
+            component="a"
+            target="_blank"
+            variant="link"
+            icon={<ExternalLinkAltIcon />}
+            iconPosition="right"
+            isInline
+            href="https://access.redhat.com/articles/rhc"
+          >
+            Registering with remote host configuration
+          </Button>
         </TextContent>
       ),
       condition: {
