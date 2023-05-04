29a8f7c76156ba9698392b6d0389b55790679d34
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2023-05-04 17:13:47 -0400

    Add Zero State

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,9 @@
-import React, { lazy } from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 
+import { Bullseye, Spinner } from '@patternfly/react-core';
+import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
+import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 
 import ShareImageModal from './Components/ShareImageModal/ShareImageModal';
@@ -8,14 +12,52 @@ const LandingPage = lazy(() => import('./Components/LandingPage/LandingPage'));
 const CreateImageWizard = lazy(() =>
   import('./Components/CreateImageWizard/CreateImageWizard')
 );
+const INVENTORY_TOTAL_FETCH_URL = '/api/inventory/v1/hosts';
 
 export const Router = () => {
+  const [hasSystems, setHasSystems] = useState(true);
+
+  useEffect(() => {
+    try {
+      axios
+        .get(`${INVENTORY_TOTAL_FETCH_URL}?page=1&per_page=1`)
+        .then(({ data }) => {
+          setHasSystems(data.total > 0);
+        });
+    } catch (e) {
+      /*eslint-disable no-console*/
+      console.log(e);
+      /*eslint-enable no-console*/
+    }
+  }, [hasSystems]);
+
   return (
-    <Routes>
-      <Route path="*" element={<LandingPage />}>
-        <Route path="imagewizard/:composeId?" element={<CreateImageWizard />} />
-        <Route path="share/:composeId" element={<ShareImageModal />} />
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={
+        <Bullseye>
+          <Spinner size="xl" />
+        </Bullseye>
+      }
+    >
+      {!hasSystems ? (
+        <AsyncComponent
+          appName="dashboard"
+          module="./AppZeroState"
+          scope="dashboard"
+          ErrorComponent={<ErrorState />}
+          app="Images"
+        />
+      ) : (
+        <Routes>
+          <Route path="*" element={<LandingPage />}>
+            <Route
+              path="imagewizard/:composeId?"
+              element={<CreateImageWizard />}
+            />
+            <Route path="share/:composeId" element={<ShareImageModal />} />
+          </Route>
+        </Routes>
+      )}
+    </Suspense>
   );
 };
