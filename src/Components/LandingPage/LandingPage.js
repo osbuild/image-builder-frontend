c6e8431481784d62471a1ff5fa7b02985bd5e200
mgold1234 <mgold@redhat.com>
2023-05-30 13:34:22 +0300

    add tabs to image builder table

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 
 import {
   Alert,
@@ -6,6 +6,9 @@ import {
   Button,
   ExpandableSection,
   Popover,
+  Tabs,
+  Tab,
+  TabTitleText,
   Text,
   TextContent,
 } from '@patternfly/react-core';
@@ -36,152 +39,175 @@ export const LandingPage = () => {
   const { quickStarts } = useChrome();
   const { isBeta } = useGetEnvironment();
   const activateQuickstart = (qs) => () => quickStarts.activateQuickstart(qs);
+  const [activeTabKey, setActiveTabkey] = useState(0);
+  const handleTabClick = (_event, tabIndex) => setActiveTabkey(tabIndex);
+  const imageBuilderTab = (
+    <section className="pf-l-page__main-section pf-c-page__main-section">
+      {!isBeta() && showBetaAlert && (
+        <Alert
+          className="pf-u-mb-xl"
+          isInline
+          variant="default"
+          title="Try new features in our Preview environment."
+          actionClose={
+            <AlertActionCloseButton onClose={() => setShowBetaAlert(false)} />
+          }
+          actionLinks={
+            <Button
+              isInline
+              component="a"
+              variant="link"
+              href="/preview/insights/image-builder/landing"
+            >
+              Enter Preview environment
+            </Button>
+          }
+        >
+          <p>
+            Launch Amazon Web Services or Microsoft Azure hosts to the cloud
+            from the console.
+          </p>
+          <p>
+            Link custom repositories and build any supported image with custom
+            content.
+          </p>
+        </Alert>
+      )}
+      {isBeta() && (
+        <ExpandableSection
+          className="pf-m-light pf-u-mb-xl expand-section"
+          toggleText="Help get started with Preview features"
+          onToggle={setShowHint}
+          isExpanded={showHint}
+          displaySize="large"
+        >
+          <p className="pf-u-pb-sm">
+            For help getting started, access the quick starts for our Preview
+            features.
+          </p>
+          <p className="pf-u-pt-sm">
+            <Button
+              icon={<ArrowRightIcon />}
+              iconPosition="right"
+              variant="link"
+              isInline
+              component="a"
+              onClick={activateQuickstart('insights-launch-aws')}
+              className="pf-u-font-weight-bold"
+            >
+              Launch an AWS Image
+            </Button>
+          </p>
+          <p className="pf-u-pt-sm">
+            <Button
+              icon={<ArrowRightIcon />}
+              iconPosition="right"
+              variant="link"
+              isInline
+              component="a"
+              onClick={activateQuickstart('insights-launch-azure')}
+              className="pf-u-font-weight-bold"
+            >
+              Launch an Azure Image
+            </Button>
+          </p>
+          <p className="pf-u-pt-sm">
+            <Button
+              icon={<ArrowRightIcon />}
+              iconPosition="right"
+              variant="link"
+              isInline
+              component="a"
+              onClick={activateQuickstart('insights-custom-repos')}
+              className="pf-u-font-weight-bold"
+            >
+              Build an Image with Custom Content
+            </Button>
+          </p>
+        </ExpandableSection>
+      )}
+      <ImagesTable />
+    </section>
+  );
 
   return (
-    <React.Fragment>
-      <PageHeader>
-        <PageHeaderTitle className="title" title="Image Builder" />
-        <Popover
-          headerContent={'About Image Builder'}
-          bodyContent={
-            <TextContent>
-              <Text>
-                Image Builder is a service that allows you to create RHEL images
-                and push them to cloud environments.
-              </Text>
-              <DocumentationButton />
-            </TextContent>
-          }
-        >
-          <Button
-            variant="plain"
-            aria-label="About image builder"
-            className="pf-u-pl-sm header-button"
-          >
-            <HelpIcon />
-          </Button>
-        </Popover>
-        <Popover
-          headerContent={'About open source'}
-          bodyContent={
-            <TextContent>
-              <Text>
-                This service is open source, so all of its code is inspectable.
-                Explore repositories to view and contribute to the source code.
-              </Text>
-              <Button
-                component="a"
-                target="_blank"
-                variant="link"
-                icon={<ExternalLinkAltIcon />}
-                iconPosition="right"
-                isInline
-                href={
-                  'https://www.osbuild.org/guides/image-builder-service/architecture.html'
-                }
-              >
-                Repositories
-              </Button>
-            </TextContent>
-          }
-        >
-          <Button
-            variant="plain"
-            aria-label="About Open Services"
-            className="pf-u-pl-sm header-button"
-          >
-            <CodeBranchIcon />
-          </Button>
-        </Popover>
-      </PageHeader>
-      <section className="pf-l-page__main-section pf-c-page__main-section">
-        {!isBeta() && showBetaAlert && (
-          <Alert
-            className="pf-u-mb-xl"
-            isInline
-            variant="default"
-            title="Try new features in our Preview environment."
-            actionClose={
-              <AlertActionCloseButton onClose={() => setShowBetaAlert(false)} />
-            }
-            actionLinks={
-              <Button
-                isInline
-                component="a"
-                variant="link"
-                href="/preview/insights/image-builder/landing"
-              >
-                Enter Preview environment
-              </Button>
+    <Suspense fallback="loading...">
+      <React.Fragment>
+        <PageHeader>
+          <PageHeaderTitle className="title" title="Image Builder" />
+          <Popover
+            headerContent={'About Image Builder'}
+            bodyContent={
+              <TextContent>
+                <Text>
+                  Image Builder is a service that allows you to create RHEL
+                  images and push them to cloud environments.
+                </Text>
+                <DocumentationButton />
+              </TextContent>
             }
           >
-            <p>
-              Launch Amazon Web Services or Microsoft Azure hosts to the cloud
-              from the console.
-            </p>
-            <p>
-              Link custom repositories and build any supported image with custom
-              content.
-            </p>
-          </Alert>
-        )}
-        {isBeta() && (
-          <ExpandableSection
-            className="pf-m-light pf-u-mb-xl expand-section"
-            toggleText="Help get started with Preview features"
-            onToggle={setShowHint}
-            isExpanded={showHint}
-            displaySize="large"
+            <Button
+              variant="plain"
+              aria-label="About image builder"
+              className="pf-u-pl-sm header-button"
+            >
+              <HelpIcon />
+            </Button>
+          </Popover>
+          <Popover
+            headerContent={'About open source'}
+            bodyContent={
+              <TextContent>
+                <Text>
+                  This service is open source, so all of its code is
+                  inspectable. Explore repositories to view and contribute to
+                  the source code.
+                </Text>
+                <Button
+                  component="a"
+                  target="_blank"
+                  variant="link"
+                  icon={<ExternalLinkAltIcon />}
+                  iconPosition="right"
+                  isInline
+                  href={
+                    'https://www.osbuild.org/guides/image-builder-service/architecture.html'
+                  }
+                >
+                  Repositories
+                </Button>
+              </TextContent>
+            }
           >
-            <p className="pf-u-pb-sm">
-              For help getting started, access the quick starts for our Preview
-              features.
-            </p>
-            <p className="pf-u-pt-sm">
-              <Button
-                icon={<ArrowRightIcon />}
-                iconPosition="right"
-                variant="link"
-                isInline
-                component="a"
-                onClick={activateQuickstart('insights-launch-aws')}
-                className="pf-u-font-weight-bold"
-              >
-                Launch an AWS Image
-              </Button>
-            </p>
-            <p className="pf-u-pt-sm">
-              <Button
-                icon={<ArrowRightIcon />}
-                iconPosition="right"
-                variant="link"
-                isInline
-                component="a"
-                onClick={activateQuickstart('insights-launch-azure')}
-                className="pf-u-font-weight-bold"
-              >
-                Launch an Azure Image
-              </Button>
-            </p>
-            <p className="pf-u-pt-sm">
-              <Button
-                icon={<ArrowRightIcon />}
-                iconPosition="right"
-                variant="link"
-                isInline
-                component="a"
-                onClick={activateQuickstart('insights-custom-repos')}
-                className="pf-u-font-weight-bold"
-              >
-                Build an Image with Custom Content
-              </Button>
-            </p>
-          </ExpandableSection>
-        )}
-        <ImagesTable />
-      </section>
-      <Outlet />
-    </React.Fragment>
+            <Button
+              variant="plain"
+              aria-label="About Open Services"
+              className="pf-u-pl-sm header-button"
+            >
+              <CodeBranchIcon />
+            </Button>
+          </Popover>
+        </PageHeader>
+        <Tabs
+          className="pf-u-ml-md"
+          activeKey={activeTabKey}
+          onSelect={handleTabClick}
+        >
+          <Tab
+            eventKey={0}
+            title={<TabTitleText>Traditional (RPM - DNF)</TabTitleText>}
+          >
+            {imageBuilderTab}
+          </Tab>
+          <Tab
+            eventKey={1}
+            title={<TabTitleText>Immutable - (OSTree)</TabTitleText>}
+          ></Tab>
+        </Tabs>
+        <Outlet />
+      </React.Fragment>
+    </Suspense>
   );
 };
 
