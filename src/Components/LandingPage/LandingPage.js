10de5b818e765c4a6ef59fa8fb3b9679a487b876
mgold1234 <mgold@redhat.com>
2023-05-30 14:50:29 +0300

    add flag to image builder

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -24,6 +24,7 @@ import {
   PageHeaderTitle,
 } from '@redhat-cloud-services/frontend-components';
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
+import { useFlag } from '@unleash/proxy-client-react';
 import { Outlet } from 'react-router-dom';
 
 import './LandingPage.scss';
@@ -41,6 +42,7 @@ export const LandingPage = () => {
   const activateQuickstart = (qs) => () => quickStarts.activateQuickstart(qs);
   const [activeTabKey, setActiveTabkey] = useState(0);
   const handleTabClick = (_event, tabIndex) => setActiveTabkey(tabIndex);
+  const edgeParityFlag = useFlag('edgeParity.image-list');
   const imageBuilderTab = (
     <section className="pf-l-page__main-section pf-c-page__main-section">
       {!isBeta() && showBetaAlert && (
@@ -189,22 +191,26 @@ export const LandingPage = () => {
             </Button>
           </Popover>
         </PageHeader>
-        <Tabs
-          className="pf-u-ml-md"
-          activeKey={activeTabKey}
-          onSelect={handleTabClick}
-        >
-          <Tab
-            eventKey={0}
-            title={<TabTitleText>Traditional (RPM - DNF)</TabTitleText>}
+        {!edgeParityFlag ? (
+          <Tabs
+            className="pf-u-ml-md"
+            activeKey={activeTabKey}
+            onSelect={handleTabClick}
           >
-            {imageBuilderTab}
-          </Tab>
-          <Tab
-            eventKey={1}
-            title={<TabTitleText>Immutable - (OSTree)</TabTitleText>}
-          ></Tab>
-        </Tabs>
+            <Tab
+              eventKey={0}
+              title={<TabTitleText>Traditional (RPM - DNF)</TabTitleText>}
+            >
+              {imageBuilderTab}
+            </Tab>
+            <Tab
+              eventKey={1}
+              title={<TabTitleText>Immutable - (OSTree)</TabTitleText>}
+            ></Tab>
+          </Tabs>
+        ) : (
+          imageBuilderTab
+        )}
         <Outlet />
       </React.Fragment>
     </Suspense>
