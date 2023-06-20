795aa99fc368b4bfe14894ed1f2e329d24dca2eb
mgold1234 <mgold@redhat.com>
2023-06-20 14:07:14 +0300

    imagesTable: capitalize all release values
    This commit change the value of release column to be consistently,
    and always show with Capital letter
    related to issue - https://github.com/RedHatInsights/image-builder-frontend/issues/946

--- types/api/index.ts
+++ types/api/index.ts
@@ -1,14 +1,20 @@
+import { components as contentSourcesComponents } from './contentSourcesSchema';
 import { components as imageBuilderComponents } from './imageBuilderSchema';
 import { components as provisioningComponents } from './provisioningSchema';
 import { components as rhsmComponents } from './rhsmSchema';
-import { components as contentSourcesComponents } from './contentSourcesSchema';
 
-export type ActivationKeys = rhsmComponents["schemas"]["ActivationKeys"];
-export type Architectures = imageBuilderComponents["schemas"]["Architectures"];
-export type ClonesResponse = imageBuilderComponents["schemas"]["ClonesResponse"];
-export type ComposeStatus = imageBuilderComponents["schemas"]["ComposeStatus"];
-export type ComposesResponse = imageBuilderComponents["schemas"]["ComposesResponse"];
-export type UploadStatus = imageBuilderComponents["schemas"]["UploadStatus"];
-export type SourceResponse = provisioningComponents["schemas"]["v1.SourceResponse"];
-export type SourceUploadInfoResponse = provisioningComponents["schemas"]["v1.SourceUploadInfoResponse"]
-export type RepositoryCollectionResponse = contentSourcesComponents["schemas"]["api.RepositoryCollectionResponse"]
+export type ActivationKeys = rhsmComponents['schemas']['ActivationKeys'];
+export type Architectures = imageBuilderComponents['schemas']['Architectures'];
+export type ClonesResponse =
+  imageBuilderComponents['schemas']['ClonesResponse'];
+export type ComposeStatus = imageBuilderComponents['schemas']['ComposeStatus'];
+export type ComposesResponse =
+  imageBuilderComponents['schemas']['ComposesResponse'];
+export type UploadStatus = imageBuilderComponents['schemas']['UploadStatus'];
+export type SourceResponse =
+  provisioningComponents['schemas']['v1.SourceResponse'];
+export type SourceUploadInfoResponse =
+  provisioningComponents['schemas']['v1.SourceUploadInfoResponse'];
+export type RepositoryCollectionResponse =
+  contentSourcesComponents['schemas']['api.RepositoryCollectionResponse'];
+export type Distributions = imageBuilderComponents['schemas']['Distributions'];
