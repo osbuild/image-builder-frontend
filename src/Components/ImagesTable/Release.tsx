834c9d83338f8578a6422375a53ee111bff5dd74
lucasgarfield <lucas@redhat.com>
2023-07-27 14:50:38 +0200

    API: Remove `/types` directory
    All Typescript types are now found in the programatically generated API
    slices in `/src/store`.

--- src/Components/ImagesTable/Release.tsx
+++ src/Components/ImagesTable/Release.tsx
@@ -1,4 +1,6 @@
-import { Distributions } from '../../../types';
+import React from 'react';
+
+import {Distributions} from '../../store/imageBuilderApi';
 
 type ReleaseProps = {
   release: Distributions;
@@ -7,12 +9,14 @@ type ReleaseProps = {
 const Release = ({ release }: ReleaseProps) => {
   const releaseDisplayValue = {
     'rhel-8': 'RHEL 8',
+    'rhel-8-nightly': 'RHEL 8',
     'rhel-84': 'RHEL 8.4',
     'rhel-85': 'RHEL 8.5',
     'rhel-86': 'RHEL 8.6',
     'rhel-87': 'RHEL 8.7',
     'rhel-88': 'RHEL 8.8',
     'rhel-9': 'RHEL 9',
+    'rhel-9-nightly': 'RHEL 9',
     'rhel-90': 'RHEL 9.0',
     'rhel-91': 'RHEL 9.1',
     'rhel-92': 'RHEL 9.2',
@@ -22,6 +26,7 @@ const Release = ({ release }: ReleaseProps) => {
     'fedora-36': 'Fedora 36',
     'fedora-37': 'Fedora 37',
     'fedora-38': 'Fedora 38',
+    'fedora-39': 'Fedora 39',
   };
 
   return releaseDisplayValue[release];
