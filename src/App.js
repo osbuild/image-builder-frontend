7df6ff7df373cecd6a3f5aaaa453c90874994c1a
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-07-31 13:34:46 +0200

    src/App.js: Remove obsolotete APP_NAVIGATION listener
    v6 of react-router-dom is now supported everywhere, and leaving the
    event listener can cause navigation bugs.
    
    See gkarat/ocp-advisor-frontend#101 as an example.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import React, { useEffect } from 'react';
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import { useStore } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import '@patternfly/patternfly/patternfly-addons.css';
 
 import { Router } from './Router';
-import { resolveRelPath } from './Utilities/path';
 
 const App = (props) => {
-  const navigate = useNavigate();
   const store = useStore();
-
-  const { hideGlobalFilter, on, updateDocumentTitle } = useChrome();
+  const { hideGlobalFilter, updateDocumentTitle } = useChrome();
 
   useEffect(() => {
     updateDocumentTitle('Image Builder | Red Hat Insights');
     hideGlobalFilter();
-    const unregister = on('APP_NAVIGATION', () => navigate(resolveRelPath('')));
-    return () => {
-      unregister();
-    };
   }, []);
 
   return (
--- src/Utilities/path.js
+++ src/Utilities/path.js
@@ -1,19 +1,5 @@
-function getBaseName(pathname) {
-  let release = '/';
-  const pathName = pathname.split('/');
-
-  pathName.shift();
-
-  if (pathName[0] === 'beta') {
-    pathName.shift();
-    release = `/beta/`;
-  }
-
-  return `${release}`;
-}
-
 function resolveRelPath(path = '') {
   return `/insights/image-builder${path.length > 0 ? `/${path}` : ''}`;
 }
 
-export { getBaseName, resolveRelPath };
+export { resolveRelPath };
