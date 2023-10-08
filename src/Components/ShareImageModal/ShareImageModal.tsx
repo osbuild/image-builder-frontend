cf1d4775442b4de7cac10d91f96e6061f747512a
jkozol <jkozol@redhat.com>
2023-10-09 00:41:50 +0200

    ShareImageModal: update the typeahead selector to pf5
    This dropdown needed to be update as part of the overall pf5 upgrade.
    Each of the components in the select now need to be declared and
    handled. Also, some of the Select and SelectOption props come from the
    Menu props since Select inherits Menu.

--- src/Components/ShareImageModal/ShareImageModal.tsx
+++ src/Components/ShareImageModal/ShareImageModal.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 
 import { Modal } from '@patternfly/react-core';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -11,19 +11,8 @@ import { resolveRelPath } from '../../Utilities/path';
 const ShareToRegionsModal = () => {
   const navigate = useNavigate();
   const handleClose = () => navigate(resolveRelPath(''));
-  const [isOpen, setIsOpen] = useState(false);
 
   const { composeId } = useParams();
-
-  const handleToggle = (isOpen: boolean) => setIsOpen(isOpen);
-
-  const handleEscapePress = () => {
-    if (isOpen) {
-      handleToggle(isOpen);
-    } else {
-      handleClose();
-    }
-  };
 
   const appendTo = useMemo(() => {
     const modalAnchor = document.querySelector(MODAL_ANCHOR);
@@ -44,16 +33,9 @@ const ShareToRegionsModal = () => {
       title="Share to new region"
       description="Configure new regions for this image that will run on your AWS. All the
         regions will launch with the same configuration."
-      onEscapePress={handleEscapePress}
       appendTo={appendTo}
     >
-      <RegionsSelect
-        composeId={composeId}
-        handleClose={handleClose}
-        handleToggle={handleToggle}
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      />
+      <RegionsSelect composeId={composeId} handleClose={handleClose} />
     </Modal>
   );
 };
