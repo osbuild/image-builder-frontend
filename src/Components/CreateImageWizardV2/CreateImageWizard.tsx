6e2a6d86e9ece94bff84ffa4c6eb82fea4aa9cf4
Thomas Lavocat <tlavocat@redhat.com>
2023-11-17 10:28:30 +0100

    wizard/footer: disable back on first step
    The first step of the wizard shouldn't allow to go back. Pass in a new
    isBackDisabled property to say if the back button should or shouldn't be
    disabled.

--- src/Components/CreateImageWizardV2/CreateImageWizard.tsx
+++ src/Components/CreateImageWizardV2/CreateImageWizard.tsx
@@ -57,13 +57,17 @@ const isIdenticalToPrev = (
 };
 
 type CustomWizardFooterPropType = {
-  isNextDisabled: boolean;
+  isNextDisabled?: boolean;
+  isBackDisabled?: boolean;
 };
 /**
  * The custom wizard footer is only switching the order of the buttons compared
  * to the default wizard footer from the PF5 library.
  */
-const CustomWizardFooter = ({ isNextDisabled }: CustomWizardFooterPropType) => {
+const CustomWizardFooter = ({
+  isNextDisabled = false,
+  isBackDisabled = false,
+}: CustomWizardFooterPropType) => {
   const { goToNextStep, goToPrevStep, close } = useWizardContext();
   return (
     <WizardFooterWrapper>
@@ -74,7 +78,11 @@ const CustomWizardFooter = ({ isNextDisabled }: CustomWizardFooterPropType) => {
       >
         Next
       </Button>
-      <Button variant="secondary" onClick={goToPrevStep}>
+      <Button
+        variant="secondary"
+        onClick={goToPrevStep}
+        isDisabled={isBackDisabled}
+      >
         Back
       </Button>
       <Button variant="link" onClick={close}>
@@ -184,6 +192,7 @@ const CreateImageWizard = () => {
             footer={
               <CustomWizardFooter
                 isNextDisabled={!hasUserSelectedAtLeastOneEnv(environment)}
+                isBackDisabled
               />
             }
           >
