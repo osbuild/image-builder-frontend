788a63ead76dedce2d2ca4f00d2548087a826cbd
lucasgarfield <lucas@redhat.com>
2024-01-06 09:12:06 +0100

    V2Wizard: Add listeners for `<TargetEnvironment>` in Image Output step
    Listeners are used to monitor changes to the release (aka distribution)
    and architecture. If either of these values is changed, the listener
    will correct the list of target environments by removing unsupported
    targets. e.g. aarch64 images do not support azure, if azure is a
    selected target and the arch is changed from x86 to aarch64, azure will
    be removed from the list of selected targets.

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,9 +5,10 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { contentSourcesApi } from './contentSourcesApi';
 import { edgeApi } from './edgeApi';
 import { imageBuilderApi } from './enhancedImageBuilderApi';
+import { listenerMiddleware, startAppListening } from './listenerMiddleware';
 import { provisioningApi } from './provisioningApi';
 import { rhsmApi } from './rhsmApi';
-import wizardSlice from './wizardSlice';
+import wizardSlice, {changeArchitecture, changeDistribution, changeImageTypes, selectArchitecture, selectDistribution, selectImageTypes} from './wizardSlice';
 
 export const reducer = {
   [contentSourcesApi.reducerPath]: contentSourcesApi.reducer,
@@ -19,14 +20,72 @@ export const reducer = {
   wizard: wizardSlice,
 };
 
+startAppListening({
+  actionCreator: changeArchitecture,
+  effect: (action, listenerApi) => {
+    const state = listenerApi.getState();
+
+    const distribution = selectDistribution(state);
+    const imageTypes = selectImageTypes(state);
+    const architecture = action.payload;
+
+    // The response from the RTKQ getArchitectures hook
+    const architecturesResponse =
+      imageBuilderApi.endpoints.getArchitectures.select({
+        distribution: distribution,
+      })(state);
+
+    const allowedImageTypes = architecturesResponse?.data?.find(
+      (elem) => elem.arch === architecture
+    )?.image_types;
+
+    const filteredImageTypes = imageTypes.filter((imageType) =>
+      allowedImageTypes?.includes(imageType)
+    );
+
+    listenerApi.dispatch(changeImageTypes(filteredImageTypes));
+  },
+});
+
+startAppListening({
+  actionCreator: changeDistribution,
+  effect: (action, listenerApi) => {
+    const state = listenerApi.getState();
+
+    const distribution = action.payload;
+    const imageTypes = selectImageTypes(state);
+    const architecture = selectArchitecture(state);
+
+    // The response from the RTKQ getArchitectures hook
+    const architecturesResponse =
+      imageBuilderApi.endpoints.getArchitectures.select({
+        distribution: distribution,
+      })(state);
+
+    const allowedImageTypes = architecturesResponse?.data?.find(
+      (elem) => elem.arch === architecture
+    )?.image_types;
+
+    const filteredImageTypes = imageTypes.filter((imageType) =>
+      allowedImageTypes?.includes(imageType)
+    );
+
+    listenerApi.dispatch(changeImageTypes(filteredImageTypes));
+  },
+});
+
+// Listener middleware must be prepended according to RTK docs:
+// https://redux-toolkit.js.org/api/createListenerMiddleware#basic-usage
 export const middleware = (getDefaultMiddleware: Function) =>
-  getDefaultMiddleware().concat(
-    promiseMiddleware,
-    contentSourcesApi.middleware,
-    imageBuilderApi.middleware,
-    rhsmApi.middleware,
-    provisioningApi.middleware
-  );
+  getDefaultMiddleware()
+    .prepend(listenerMiddleware.middleware)
+    .concat(
+      promiseMiddleware,
+      contentSourcesApi.middleware,
+      imageBuilderApi.middleware,
+      rhsmApi.middleware,
+      provisioningApi.middleware
+    );
 
 export const store = configureStore({ reducer, middleware });
 
---
+++ src/store/listenerMiddleware.ts
@@ -0,0 +1,18 @@
+// listenerMiddleware.ts
+// https://redux-toolkit.js.org/api/createListenerMiddleware#typescript-usage
+import { createListenerMiddleware, addListener } from '@reduxjs/toolkit';
+import type { TypedStartListening, TypedAddListener } from '@reduxjs/toolkit';
+
+import type { RootState, AppDispatch } from './index';
+
+export const listenerMiddleware = createListenerMiddleware();
+
+export type AppStartListening = TypedStartListening<RootState, AppDispatch>;
+
+export const startAppListening =
+  listenerMiddleware.startListening as AppStartListening;
+
+export const addAppListener = addListener as TypedAddListener<
+  RootState,
+  AppDispatch
+>;
