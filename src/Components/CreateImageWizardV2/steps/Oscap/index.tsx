c4e1709de8cf2674037fb01a6aae2846c089a898
mgold1234 <mgold@redhat.com>
2024-01-11 13:19:03 +0200

    V2Wizard: add oscap to wizard2
    This adds the OpenSCAP step to the V2Wizard,
    adds new values to the wizardSlice and enables relevant tests in the `CreateImageWizard.test.tsx` test suite

---
+++ src/Components/CreateImageWizardV2/steps/Oscap/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Form, Text, Title } from '@patternfly/react-core';
+
+import { Oscap } from './Oscap';
+
+import { imageBuilderApi } from '../../../../store/enhancedImageBuilderApi';
+import { useAppSelector } from '../../../../store/hooks';
+import { selectDistribution } from '../../../../store/wizardSlice';
+import DocumentationButton from '../../../sharedComponents/DocumentationButton';
+
+const OscapStep = () => {
+  const prefetchOscapProfile = imageBuilderApi.usePrefetch('getOscapProfiles');
+  const release = useAppSelector((state) => selectDistribution(state));
+  prefetchOscapProfile({ distribution: release });
+
+  return (
+    <Form>
+      <Title headingLevel="h2">OpenSCAP profile</Title>
+      <Text>
+        Use OpenSCAP to monitor the adherence of your registered RHEL systems to
+        a selected regulatory compliance profile
+        <br />
+        <DocumentationButton />
+      </Text>
+      <Oscap />
+    </Form>
+  );
+};
+
+export default OscapStep;
