7d1d5ffa70de1f37573fe693cbd595c98ec91f0d
Ondrej Ezr <oezr@redhat.com>
2024-01-23 10:07:27 +0100

    api: split pulling API changes and regenerating new API
    For checking wether the api was tempered with, we need separate action,
    that just checks the code against the currently pulled spec.
    
    This introduces two subactions for `npx api`.
    These are `npx api:generate` and `npx api:pull`.

--- api/codegen.sh
+++ api/codegen.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Download the most up-to-date imageBuilder.yaml file and overwrite the existing one
-curl https://raw.githubusercontent.com/osbuild/image-builder/main/internal/v1/api.yaml -o ./api/schema/imageBuilder.yaml
-
 # Run commands in the background using & operator
 npx @rtk-query/codegen-openapi ./api/config/imageBuilder.ts &
 npx @rtk-query/codegen-openapi ./api/config/rhsm.ts &
