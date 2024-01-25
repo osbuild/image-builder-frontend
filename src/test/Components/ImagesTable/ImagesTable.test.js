97a75e04654e81bd4c907f1f87d09b55ca101c22
Amir <amirfefer@gmail.com>
2024-01-25 16:54:46 +0200

    feat(HMS-3392): add blueprint's version in image table

--- src/test/Components/ImagesTable/ImagesTable.test.js
+++ src/test/Components/ImagesTable/ImagesTable.test.js
@@ -19,7 +19,16 @@ jest.mock('@redhat-cloud-services/frontend-components/useChrome', () => ({
 
 jest.mock('@unleash/proxy-client-react', () => ({
   useUnleashContext: () => jest.fn(),
-  useFlag: jest.fn((flag) => (flag === 'edgeParity.image-list' ? false : true)),
+  useFlag: jest.fn((flag) => {
+    switch (flag) {
+      case 'edgeParity.image-list':
+        return false;
+      case 'image-builder.new-wizard.enabled':
+        return false;
+      default:
+        return true;
+    }
+  }),
 }));
 
 beforeAll(() => {
