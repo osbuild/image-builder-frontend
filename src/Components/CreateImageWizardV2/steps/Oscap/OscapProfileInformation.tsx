37115e1a1717b2422abe216a8a37e251031f5b2b
mgold1234 <mgold@redhat.com>
2024-01-29 14:40:11 +0200

    v2Wizrad: add kernel and services to OpenScap step
    this commit add kernel and enabled/disabled services data
    to openScap step.
    this data is read only, and user can't edit it.

--- src/Components/CreateImageWizardV2/steps/Oscap/OscapProfileInformation.tsx
+++ src/Components/CreateImageWizardV2/steps/Oscap/OscapProfileInformation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import {
+  CodeBlock,
+  CodeBlockCode,
   Spinner,
   TextContent,
   TextList,
@@ -20,7 +22,6 @@ import {
 const OscapProfileInformation = (): JSX.Element => {
   const release = useAppSelector((state) => selectDistribution(state));
   const oscapProfile = useAppSelector((state) => selectProfile(state));
-
   const {
     data: oscapProfileInfo,
     isFetching: isFetchingOscapProfileInfo,
@@ -36,6 +37,10 @@ const OscapProfileInformation = (): JSX.Element => {
     }
   );
 
+  const enabledServices = oscapProfileInfo?.services?.enabled;
+  const enabledServicesDisplayString = enabledServices?.reduce((accumulator, currentValue) => accumulator + ' ' + currentValue, '');
+  const disableServices = oscapProfileInfo?.services?.disabled;
+  const disableServicesDisplayString = disableServices?.reduce((accumulator, currentValue) => accumulator + ' ' + currentValue, '');
   return (
     <>
       {isFetchingOscapProfileInfo && <Spinner size="lg" />}
@@ -73,6 +78,47 @@ const OscapProfileInformation = (): JSX.Element => {
             </TextListItem>
             <TextListItem component={TextListItemVariants.dd}>
               {oscapProfileInfo.openscap?.profile_id}
+            </TextListItem>
+
+            <TextListItem
+                component={TextListItemVariants.dt}
+                className="pf-u-min-width"
+            >
+              Kernel:
+            </TextListItem>
+                <TextListItem component={TextListItemVariants.dd}>
+              <CodeBlock>
+                <CodeBlockCode id="code-content">{oscapProfileInfo?.kernel?.append}
+                </CodeBlockCode>
+              </CodeBlock>
+            </TextListItem>
+            <TextListItem
+                component={TextListItemVariants.dt}
+                className="pf-u-min-width"
+            >
+              Disabled Services:
+            </TextListItem>
+            <TextListItem component={TextListItemVariants.dd}>
+              <CodeBlock>
+                <CodeBlockCode id="code-content">
+                 {disableServicesDisplayString}
+
+                </CodeBlockCode>
+              </CodeBlock>
+            </TextListItem>
+
+            <TextListItem
+                component={TextListItemVariants.dt}
+                className="pf-u-min-width"
+            >
+              Enabled Services:
+            </TextListItem>
+            <TextListItem component={TextListItemVariants.dd}>
+              <CodeBlock>
+                <CodeBlockCode id="code-content">
+                  {enabledServicesDisplayString}
+                </CodeBlockCode>
+              </CodeBlock>
             </TextListItem>
           </TextList>
         </TextContent>
