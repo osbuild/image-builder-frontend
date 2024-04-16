f95601cc737d9e8f837e4b04ba5ba3d4ab68cce0
regexowl <ksimicko@redhat.com>
2024-04-16 10:50:51 +0200

    Blueprints: Surface "Edit blueprint" button
    This surfaces "Edit blueprint" button to the ImageTableToolbar and removes it from the blueprints actions menu as per recent mocks.

---
+++ src/Components/Blueprints/EditBlueprintButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Button } from '@patternfly/react-core';
+import { useNavigate } from 'react-router-dom';
+
+import { selectSelectedBlueprintId } from '../../store/BlueprintSlice';
+import { useAppSelector } from '../../store/hooks';
+import { resolveRelPath } from '../../Utilities/path';
+
+export const EditBlueprintButton = () => {
+  const navigate = useNavigate();
+  const selectedBlueprintId = useAppSelector(selectSelectedBlueprintId);
+
+  return (
+    <Button
+      ouiaId="edit-blueprint-button"
+      onClick={() =>
+        navigate(resolveRelPath(`imagewizard/${selectedBlueprintId}`))
+      }
+      variant="secondary"
+    >
+      Edit blueprint
+    </Button>
+  );
+};
