1a17b911d071fd4735cdbfb52e13340d8e42466f
regexowl <ksimicko@redhat.com>
2024-04-29 09:50:53 +0200

    V2Wizard: Remove manual partitioning for ISO only images
    This hides the "Manually configure partitions" option from the FSC step for blueprints which have only ISO selected as a target.
    
    There is already an alert in place for cases when ISO is combined with another target.

--- src/Components/CreateImageWizardV2/steps/FileSystem/index.tsx
+++ src/Components/CreateImageWizardV2/steps/FileSystem/index.tsx
@@ -8,10 +8,12 @@ import FileSystemPartition from './FileSystemPartition';
 
 import { useAppSelector } from '../../../../store/hooks';
 import { selectFileSystemPartitionMode } from '../../../../store/wizardSlice';
+import { useHasIsoTargetOnly } from '../../utilities/hasIsoTargetOnly';
 export type FileSystemPartitionMode = 'automatic' | 'manual';
 
 const FileSystemStep = () => {
   const fileSystemPartitionMode = useAppSelector(selectFileSystemPartitionMode);
+  const hasIsoTargetOnly = useHasIsoTargetOnly();
 
   return (
     <Form>
@@ -19,7 +21,9 @@ const FileSystemStep = () => {
         File system configuration
       </Title>
       <Text>Define the partitioning of the image</Text>
-      {fileSystemPartitionMode === 'automatic' ? (
+      {hasIsoTargetOnly ? (
+        <FileSystemAutomaticPartition />
+      ) : fileSystemPartitionMode === 'automatic' ? (
         <>
           <FileSystemPartition />
           <FileSystemAutomaticPartition />
