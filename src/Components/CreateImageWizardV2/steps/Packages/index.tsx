db4e80eb67c44bcd9e3e91346eb643bf77f5c49b
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-06-14 13:29:57 +0200

    CreateImageWizardV2: add info alert to packages step
    Inform users how to search for package groups.

--- src/Components/CreateImageWizardV2/steps/Packages/index.tsx
+++ src/Components/CreateImageWizardV2/steps/Packages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Text, Form, Title } from '@patternfly/react-core';
+import { Alert, Text, Form, Title } from '@patternfly/react-core';
 import { useFlag } from '@unleash/proxy-client-react';
 
 import PackageRecommendations from './PackageRecommendations';
@@ -21,6 +21,11 @@ const PackagesStep = () => {
         Additional packages
       </Title>
       <Text>Blueprints created with Images include all required packages.</Text>
+      <Alert variant="info" isInline title="Search for package groups">
+        Search for package groups by starting your search with the &apos;@&apos;
+        character. A single &apos;@&apos; as search input lists all available
+        package groups.
+      </Alert>
       <Packages />
       {isBeta() && packageRecommendationsFlag && distribution !== CENTOS_9 && (
         <PackageRecommendations />
