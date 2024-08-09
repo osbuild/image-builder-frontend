4fa2d34bb2ea968f10d9d0a392663fda53871141
regexowl <ksimicko@redhat.com>
2024-08-09 15:40:11 +0200

    testUtils: Merge `renderCustomRoutesWithReduxRouter` and `renderWithReduxRouter`
    `renderCustomRoutesWithReduxRouter` and `renderWithReduxRouter`are functionally the same apart from routes they use to render components.
    
    We could just use `renderCustomRoutesWithReduxRouter` and use the routes from `renderWithReduxRouter` as a default value for an optional parameter `routes`.

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -12,46 +12,27 @@ import ShareImageModal from '../Components/ShareImageModal/ShareImageModal';
 import { middleware, reducer } from '../store';
 import { resolveRelPath } from '../Utilities/path';
 
+const defaultRoutes = [
+  {
+    path: 'insights/image-builder/*',
+    element: <LandingPage />,
+  },
+  {
+    path: 'insights/image-builder/imagewizard/import',
+    element: <ImportImageWizard />,
+  },
+  {
+    path: 'insights/image-builder/share/:composeId',
+    element: <ShareImageModal />,
+  },
+];
+
 export const renderCustomRoutesWithReduxRouter = async (
   route = '/',
   preloadedState = {},
-  routes
+  routes = defaultRoutes
 ) => {
   const store = configureStore({ reducer, middleware, preloadedState });
-
-  const router = createMemoryRouter(routes, {
-    initialEntries: [resolveRelPath(route)],
-  });
-
-  render(
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  );
-
-  return { router, store };
-};
-
-export const renderWithReduxRouter = async (
-  route = '/',
-  preloadedState = {}
-) => {
-  const store = configureStore({ reducer, middleware, preloadedState });
-
-  const routes = [
-    {
-      path: 'insights/image-builder/*',
-      element: <LandingPage />,
-    },
-    {
-      path: 'insights/image-builder/imagewizard/import',
-      element: <ImportImageWizard />,
-    },
-    {
-      path: 'insights/image-builder/share/:composeId',
-      element: <ShareImageModal />,
-    },
-  ];
 
   const router = createMemoryRouter(routes, {
     initialEntries: [resolveRelPath(route)],
