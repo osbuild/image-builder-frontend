e7cfc7069586f5805000dc69eeeb76babb5f964a
Anna Vítová <avitova@redhat.com>
2024-10-08 16:42:11 +0300

    update compliance and content sources api

--- api/pull.sh
+++ api/pull.sh
@@ -4,3 +4,5 @@
 curl https://raw.githubusercontent.com/osbuild/image-builder/main/internal/v1/api.yaml -o ./api/schema/imageBuilder.yaml
 
 curl https://console.redhat.com/api/compliance/v2/openapi.json -o ./api/schema/compliance.json
+
+curl https://console.redhat.com/api/content-sources/v1/openapi.json -o ./api/schema/contentSources.json
