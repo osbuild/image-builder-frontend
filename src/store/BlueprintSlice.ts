076fb3fc9d8aafeba5d2aabe81375d6a14c97a30
regexowl <ksimicko@redhat.com>
2024-10-10 14:09:48 +0200

    src: Resolve type-only imports

--- src/store/BlueprintSlice.ts
+++ src/store/BlueprintSlice.ts
@@ -1,6 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-import { RootState } from '.';
+import type { RootState } from '.';
 
 export type versionFilterType = 'latest' | 'all';
 
