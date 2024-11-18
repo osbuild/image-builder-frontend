b8b9b8f846dc8905ff086ef47d445c7f97ced804
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-11-18 13:10:58 +0100

    src/Utilities: RHEL 10 support for isRhel

--- src/Utilities/isRhel.ts
+++ src/Utilities/isRhel.ts
@@ -1,10 +1,18 @@
-import { RHEL_8, RHEL_9, RHEL_9_BETA } from '../constants';
+import {
+  RHEL_8,
+  RHEL_9,
+  RHEL_9_BETA,
+  RHEL_10,
+  RHEL_10_BETA,
+} from '../constants';
 
 function isRhel(distro: string) {
   switch (distro) {
     case RHEL_8:
     case RHEL_9:
     case RHEL_9_BETA:
+    case RHEL_10:
+    case RHEL_10_BETA:
       return true;
     default:
       return false;
