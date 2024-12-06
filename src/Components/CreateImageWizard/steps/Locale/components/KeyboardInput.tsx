97877114f6e1e7413826bd5b77db3b9cc721a127
regexowl <ksimicko@redhat.com>
2024-12-06 11:21:11 +0100

    Wizard: Add Keyboard drop down
    This adds a drop down for keyboard selection. The options are populated with an output of `localectl list-keymaps`.

--- src/Components/CreateImageWizard/steps/Locale/components/KeyboardInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { FormGroup } from '@patternfly/react-core';
-
-const KeyboardInput = () => {
-  return <FormGroup isRequired={false} label="Keyboard"></FormGroup>;
-};
-
-export default KeyboardInput;
