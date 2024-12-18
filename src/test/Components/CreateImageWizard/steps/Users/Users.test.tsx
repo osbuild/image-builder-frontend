cdbd38c815965038d5591fd18af91677a490433e
Michal Gold <mgold@redhat.com>
2024-12-18 14:52:53 +0200

    test: Add create mode test for User step
    This adds test that check the functionality of blueprint request with user.

--- src/test/Components/CreateImageWizard/steps/Users/Users.test.tsx
+++ src/test/Components/CreateImageWizard/steps/Users/Users.test.tsx
@@ -2,10 +2,15 @@ import type { Router as RemixRouter } from '@remix-run/router';
 import { screen, waitFor } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
+import { CREATE_BLUEPRINT } from '../../../../../constants';
 import {
+  blueprintRequest,
   clickBack,
   clickNext,
+  enterBlueprintName,
   getNextButton,
+  interceptBlueprintRequest,
+  openAndDismissSaveAndBuildModal,
   verifyCancelButton,
 } from '../../wizardTestUtils';
 import {
@@ -23,6 +28,36 @@ const goToUsersStep = async () => {
   await clickNext();
   await clickNext();
   await clickNext();
+};
+
+const goToReviewStep = async () => {
+  await clickNext(); // Timezone
+  await clickNext(); // Locale
+  await clickNext(); // Hostname
+  await clickNext(); // Kernel
+  await clickNext(); // First boot
+  await clickNext(); // Details
+  await enterBlueprintName();
+  await clickNext(); // Review
+};
+
+const addValidUser = async () => {
+  const user = userEvent.setup();
+  const addUser = await screen.findByRole('button', { name: /add a user/i });
+  expect(addUser).toBeEnabled();
+  await waitFor(() => user.click(addUser));
+  const enterUserName = screen.getByRole('textbox', {
+    name: /blueprint user name/i,
+  });
+  const nextButton = await getNextButton();
+  await waitFor(() => user.type(enterUserName, 'best'));
+  await waitFor(() => expect(enterUserName).toHaveValue('best'));
+  const enterUserPassword = screen.getByPlaceholderText(/enter password/i);
+  await waitFor(() => user.type(enterUserPassword, 'bestPASSWORD11'));
+  const enterUserConfirmPassword =
+    screen.getByPlaceholderText(/confirm password/i);
+  await waitFor(() => user.type(enterUserConfirmPassword, 'bestPASSWORD11'));
+  await waitFor(() => expect(nextButton).toBeEnabled());
 };
 
 describe('Step Users', () => {
@@ -81,27 +116,44 @@ describe('Step Users', () => {
   });
 
   test('with valid name', async () => {
-    const user = userEvent.setup();
     await renderCreateMode();
     await goToRegistrationStep();
     await clickRegisterLater();
     await goToUsersStep();
-    const addUser = await screen.findByRole('button', { name: /add a user/i });
-    expect(addUser).toBeEnabled();
-    await waitFor(() => user.click(addUser));
-    const enterUserName = screen.getByRole('textbox', {
-      name: /blueprint user name/i,
+    await addValidUser();
+    await goToReviewStep();
+  });
+
+  describe('User request generated correctly', () => {
+    test('with valid name and password', async () => {
+      await renderCreateMode();
+      await goToRegistrationStep();
+      await clickRegisterLater();
+      await goToUsersStep();
+      await addValidUser();
+      await goToReviewStep();
+      // informational modal pops up in the first test only as it's tied
+      // to a 'imageBuilder.saveAndBuildModalSeen' variable in localStorage
+      await openAndDismissSaveAndBuildModal();
+      const receivedRequest = await interceptBlueprintRequest(CREATE_BLUEPRINT);
+
+      const expectedRequest = {
+        ...blueprintRequest,
+        customizations: {
+          users: [
+            {
+              name: 'best',
+              password: 'bestPASSWORD11',
+              groups: [],
+              ssh_key: '',
+            },
+          ],
+        },
+      };
+
+      await waitFor(() => {
+        expect(receivedRequest).toEqual(expectedRequest);
+      });
     });
-    const nextButton = await getNextButton();
-    await waitFor(() => user.type(enterUserName, 'best'));
-    await waitFor(() => expect(enterUserName).toHaveValue('best'));
-    const enterUserPassword = screen.getByPlaceholderText(/enter password/i);
-    await waitFor(() => user.type(enterUserPassword, 'bestPASSWORD11'));
-    const enterUserConfirmPassword =
-      screen.getByPlaceholderText(/confirm password/i);
-    await waitFor(() => user.type(enterUserConfirmPassword, 'bestPASSWORD11'));
-    await waitFor(() => expect(nextButton).toBeEnabled());
-    await waitFor(() => user.click(nextButton));
-    await screen.findByText('Select a timezone for your image.');
   });
 });
