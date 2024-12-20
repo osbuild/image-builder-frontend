3e6985988a04946594b22d2a96965da2f2dd0db6
Gianluca Zuccarelli <gzuccare@redhat.com>
2024-12-20 14:02:02 +0000

    store/backendApi: export the relevant store api
    Since the implementation of the cockpitApi and imageBuilderApi are different,
    we should make sure to export the correct one depending on the environment
    we are in.

--- src/store/backendApi.ts
+++ src/store/backendApi.ts
@@ -1,6 +1,22 @@
-import { useGetBlueprintsQuery as useCockpitGetBlueprintsQuery } from './cockpitApi';
-import { useGetBlueprintsQuery as useImageBuilderGetBlueprintsQuery } from './imageBuilderApi';
+import {
+  useGetBlueprintsQuery as useCockpitGetBlueprintsQuery,
+  useDeleteBlueprintMutation as useCockpitDeleteMutation,
+} from './cockpitApi';
+import { cockpitApi } from './enhancedCockpitApi';
+import { imageBuilderApi } from './enhancedImageBuilderApi';
+import {
+  useGetBlueprintsQuery as useImageBuilderGetBlueprintsQuery,
+  useDeleteBlueprintMutation as useImageBuilderDeleteMutation,
+} from './imageBuilderApi';
 
 export const useGetBlueprintsQuery = process.env.IS_ON_PREMISE
   ? useCockpitGetBlueprintsQuery
   : useImageBuilderGetBlueprintsQuery;
+
+export const useDeleteBlueprintMutation = process.env.IS_ON_PREMISE
+  ? useCockpitDeleteMutation
+  : useImageBuilderDeleteMutation;
+
+export const backendApi = process.env.IS_ON_PREMISE
+  ? cockpitApi
+  : imageBuilderApi;
