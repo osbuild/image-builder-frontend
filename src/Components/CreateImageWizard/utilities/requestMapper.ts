b8dd435257c75448d9ddca9a1b45d405f8321a15
regexowl <ksimicko@redhat.com>
2025-01-07 15:52:25 +0100

    Wizard: Parse old snapshot dates when editing
    This updates format of the snapshot date when the blueprint with the old format gets edited.

--- src/Components/CreateImageWizard/utilities/requestMapper.ts
+++ src/Components/CreateImageWizard/utilities/requestMapper.ts
@@ -173,9 +173,22 @@ function commonRequestToState(
     (image) => image.image_type === 'azure'
   );
 
-  const snapshot_date =
+  const snapshotDateFromRequest =
     request.image_requests.find((image) => !!image.snapshot_date)
       ?.snapshot_date || '';
+  let snapshot_date = '';
+
+  if (
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}$/.test(
+      snapshotDateFromRequest
+    )
+  ) {
+    snapshot_date = snapshotDateFromRequest;
+  } else if (/^\d{4}-\d{2}-\d{2}$/.test(snapshotDateFromRequest)) {
+    snapshot_date = snapshotDateFromRequest + 'T00:00:00+00:00';
+  } else {
+    snapshot_date = '';
+  }
 
   const awsUploadOptions = aws?.upload_request
     .options as AwsUploadRequestOptions;
