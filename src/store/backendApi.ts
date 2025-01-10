d31bb45510498fea2b8d4541540cb149f429a757
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-01-10 12:43:20 +0000

    store/cockpitApi: getArchitectures endpoint
    We were making a call to a dead endpoint to get the architectures.
    Instead, create a custom query function to return the list of architectures
    and image types.

--- src/store/backendApi.ts
+++ src/store/backendApi.ts
@@ -1,13 +1,19 @@
 import {
+  useGetArchitecturesQuery as useCockpitGetArchitecturesQuery,
   useGetBlueprintsQuery as useCockpitGetBlueprintsQuery,
   useDeleteBlueprintMutation as useCockpitDeleteMutation,
 } from './cockpitApi';
 import { cockpitApi } from './enhancedCockpitApi';
 import { imageBuilderApi } from './enhancedImageBuilderApi';
 import {
+  useGetArchitecturesQuery as useImageBuilderGetArchitecturesQuery,
   useGetBlueprintsQuery as useImageBuilderGetBlueprintsQuery,
   useDeleteBlueprintMutation as useImageBuilderDeleteMutation,
 } from './imageBuilderApi';
+
+export const useGetArchitecturesQuery = process.env.IS_ON_PREMISE
+  ? useCockpitGetArchitecturesQuery
+  : useImageBuilderGetArchitecturesQuery;
 
 export const useGetBlueprintsQuery = process.env.IS_ON_PREMISE
   ? useCockpitGetBlueprintsQuery
