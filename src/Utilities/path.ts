de2f054a4ed7fe7ee6e905e98278aae6c8b4c34c
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-01-10 12:34:07 +0000

    cockpit: switch to hashrouter
    The browser router doesn't seem to work inside cockpit, switching
    to the hashrouter resolves this.

--- src/Utilities/path.ts
+++ src/Utilities/path.ts
@@ -1,4 +1,9 @@
 function resolveRelPath(path = '') {
+  if (process.env.IS_ON_PREMISE) {
+    // We're using the hash router
+    // so we can just return the path
+    return path.length > 0 ? path : '/';
+  }
   return `/insights/image-builder${path.length > 0 ? `/${path}` : ''}`;
 }
 
