f7a6f027627340eae47fa836548f289cd9c2c1dc
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-01-23 18:39:20 +0000

    CreateImageWizard: initialize on-prem with host arch
    Initialize the create image wizard with the host arch for the
    on-prem frontend.

--- src/Components/CreateImageWizard/utilities/getHostInfo.ts
+++ src/Components/CreateImageWizard/utilities/getHostInfo.ts
@@ -1,3 +1,4 @@
+import cockpit from 'cockpit';
 import { read_os_release } from 'os-release';
 
 import { Distributions } from '../../../store/imageBuilderApi';
@@ -5,4 +6,13 @@ import { Distributions } from '../../../store/imageBuilderApi';
 export const getHostDistro = async () => {
   const osRel = await read_os_release();
   return `${osRel.ID}-${osRel.VERSION_ID}` as Distributions;
+};
+
+type Architecture = 'x86_64' | 'aarch64';
+export const getHostArch = async () => {
+  const hostArch = await cockpit.spawn(['uname', '-m'], {
+    superuser: 'try',
+  });
+
+  return (hostArch as string).trim() as Architecture;
 };
