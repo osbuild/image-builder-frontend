1dc657a3510d5ccb22d94230cd18a5cfd5caf192
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-01-24 13:45:23 +0100

    store/cockpitApi: invalidate cache after building an image
    With the custom query functions the cached data from getComposes and
    getBlueprintComposes doesn't get invalidated. Luckily RTKQuery provides
    a mechanism to invalidate the cache after a mutation.

--- src/store/emptyCockpitApi.ts
+++ src/store/emptyCockpitApi.ts
@@ -3,5 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const emptyCockpitApi = createApi({
   reducerPath: 'cockpitApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  tagTypes: ['Composes'],
   endpoints: () => ({}),
 });
