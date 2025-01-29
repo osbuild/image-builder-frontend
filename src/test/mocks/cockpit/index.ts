00d1ed81ef18e9ea53c5f2a870399b1dec159340
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-01-29 16:31:03 +0100

    test/mocks: expand cockpit mocks
    Use the data from the regular fixtures, but through cockpit APIs.
    
    There are a few hacks to get around the fact that there are no composes
    without blueprints, and the lack of paging support in the cockpit api.

--- src/test/mocks/cockpit/index.ts
+++ src/test/mocks/cockpit/index.ts
@@ -1,16 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { Method, Headers, Params } from '../../../store/cockpit/types';
+
+import path from 'path';
+
+import { cockpitFile } from './cockpitFile';
+import { cockpitHTTP } from './cockpitHTTP';
 
 type userinfo = {
   home: string;
-};
-
-type requestOptions = {
-  path: string;
-  method: Method;
-  body: unknown;
-  headers: Headers | undefined;
-  params: Params | undefined;
 };
 
 export default {
@@ -21,43 +17,15 @@ export default {
   user: (): Promise<userinfo> => {
     return new Promise((resolve) => {
       resolve({
-        home: '',
+        home: '/default',
       });
     });
   },
-  file: (path: string) => {
-    return {
-      read: (): Promise<string> => {
-        return new Promise((resolve) => {
-          resolve('');
-        });
-      },
-      close: () => {},
-      replace: (contents: string): Promise<void> => {
-        return new Promise((resolve) => {
-          resolve();
-        });
-      },
-    };
-  },
+  file: cockpitFile,
   spawn: (args: string[], attributes: object): Promise<string | Uint8Array> => {
     return new Promise((resolve) => {
       resolve('');
     });
   },
-  http: (address: string, options: object) => {
-    return {
-      get: (path?: string, headers?: object): string => {
-        return '';
-      },
-      post: (path: string, data: object, headers?: object): string => {
-        return '';
-      },
-      request: (request: requestOptions): Promise<string> => {
-        return new Promise((resolve) => {
-          resolve('');
-        });
-      },
-    };
-  },
+  http: cockpitHTTP,
 };
