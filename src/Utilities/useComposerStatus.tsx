994cba931662983e4bb454bb0c9a3d5bde5cdfc0
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-02-04 12:43:35 +0000

    Utilities: add useComposerStatus hook
    Add a custom hook to check to see if the osbuild-composer.socket
    is enabled & running.

---
+++ src/Utilities/useComposerStatus.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import cockpit from 'cockpit';
+
+export const useGetComposerSocketStatus = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [started, setStarted] = useState(false);
+
+  useEffect(() => {
+    const isEnabled = async () => {
+      try {
+        const result = await cockpit.spawn(
+          ['systemctl', 'is-enabled', 'osbuild-composer.socket'],
+          { superuser: 'try' }
+        );
+        setEnabled((result as string).trim() === 'enabled');
+      } catch {
+        // error code 1 means disabled
+        setEnabled(false);
+      }
+    };
+
+    const isStarted = async () => {
+      try {
+        const result = await cockpit.spawn(
+          ['systemctl', 'is-active', 'osbuild-composer.socket'],
+          { superuser: 'try' }
+        );
+        setStarted((result as string).trim() === 'active');
+      } catch {
+        // exit code 3 means not active
+        setStarted(false);
+      }
+    };
+
+    isEnabled();
+    isStarted();
+  }, []);
+
+  return {
+    enabled,
+    started,
+  };
+};
