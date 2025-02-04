68d33130f70537d7133a4c74067b0649c7d473e6
regexowl <ksimicko@redhat.com>
2025-02-04 16:28:57 +0100

    src: Align to center
    This ensures that both Landing page and Wizard are aligned to center within wide windows.

--- src/Components/CreateImageWizard/CreateImageWizard.tsx
+++ src/Components/CreateImageWizard/CreateImageWizard.tsx
@@ -307,7 +307,7 @@ const CreateImageWizard = ({ isEdit }: CreateImageWizardProps) => {
   return (
     <>
       <ImageBuilderHeader inWizard />
-      <PageSection isWidthLimited>
+      <PageSection isWidthLimited isCenterAligned>
         <Wizard
           startIndex={startIndex}
           onClose={() => navigate(resolveRelPath(''))}
