a0e4101732d639db21ee364af651a1a6f80b4823
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-02-05 14:05:59 +0100

    api: Add osbuild-composer's cloudapi

--- api/pull.sh
+++ api/pull.sh
@@ -6,3 +6,5 @@ curl https://raw.githubusercontent.com/osbuild/image-builder/main/internal/v1/ap
 curl https://console.redhat.com/api/compliance/v2/openapi.json -o ./api/schema/compliance.json
 
 curl https://console.redhat.com/api/content-sources/v1/openapi.json -o ./api/schema/contentSources.json
+
+curl https://raw.githubusercontent.com/osbuild/osbuild-composer/main/internal/cloudapi/v2/openapi.v2.yml -o ./api/schema/composerCloudApi.v2.yaml
