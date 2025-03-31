b82806eabd4e0d2434edb4c421c479d2d959f5f5
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-03-31 17:00:12 +0000

    CreateImageWizard: hide aws sources for on-prem
    For now just allow manual entry of the aws account id, since we aren't
    able to configure sources on-premise yet (if ever).

--- src/Components/CreateImageWizard/CreateImageWizard.tsx
+++ src/Components/CreateImageWizard/CreateImageWizard.tsx
@@ -78,6 +78,7 @@ import {
   selectImageTypes,
   addImageType,
   changeRegistrationType,
+  changeAwsShareMethod,
 } from '../../store/wizardSlice';
 import isRhel from '../../Utilities/isRhel';
 import { resolveRelPath } from '../../Utilities/path';
@@ -188,6 +189,10 @@ const CreateImageWizard = ({ isEdit }: CreateImageWizardProps) => {
       const arch = await getHostArch();
       dispatch(changeArchitecture(arch));
     };
+
+    if (process.env.IS_ON_PREMISE) {
+      dispatch(changeAwsShareMethod('manual'));
+    }
 
     if (process.env.IS_ON_PREMISE && !isEdit) {
       if (!searchParams.get('release')) {
