e31efdd492062e166adbd00fbaac665d752de4a7
Tom Koscielniak <tkosciel@redhat.com>
2025-04-03 17:09:37 +0200

    Add parallelization and remove unused test.describe

--- playwright/Customizations/Hostname.spec.ts
+++ playwright/Customizations/Hostname.spec.ts
@@ -15,81 +15,77 @@ import {
 } from '../helpers/wizardHelpers';
 import { ibFrame, isHosted } from '../lib/lib';
 
-test.describe.serial('Test Hostname', () => {
-  test('Create a blueprint with Hostname customization', async ({
-    page,
-    cleanup,
-  }) => {
-    const blueprintName = 'test-' + uuidv4();
-    const hostname = 'testsystem';
+test('Create a blueprint with Hostname customization', async ({
+  page,
+  cleanup,
+}) => {
+  const blueprintName = 'test-' + uuidv4();
+  const hostname = 'testsystem';
 
-    // Delete the blueprint after the run fixture
-    await cleanup.add(() => deleteBlueprint(page, blueprintName));
+  // Delete the blueprint after the run fixture
+  await cleanup.add(() => deleteBlueprint(page, blueprintName));
 
-    // Login, navigate to IB and get the frame
-    await login(page);
-    const frame = await ibFrame(page);
+  // Login, navigate to IB and get the frame
+  await login(page);
+  const frame = await ibFrame(page);
 
-    await test.step('Navigate to optional steps in Wizard', async () => {
-      await navigateToOptionalSteps(frame);
-      await registerLater(frame);
-    });
-
-    await test.step('Select and fill the Hostname step', async () => {
-      if (isHosted()) {
-        await frame.getByRole('button', { name: 'Hostname' }).click();
-      } else {
-        await frame
-          .getByRole('listitem')
-          .filter({ hasText: /^Hostname$/ })
-          .click();
-      }
-      await frame.getByRole('textbox', { name: 'hostname input' }).click();
-      await frame
-        .getByRole('textbox', { name: 'hostname input' })
-        .fill(hostname);
-      await frame.getByRole('button', { name: 'Review and finish' }).click();
-    });
-
-    await test.step('Fill the BP details', async () => {
-      await fillInDetails(frame, blueprintName);
-    });
-
-    await test.step('Create BP', async () => {
-      await createBlueprint(frame, blueprintName);
-    });
-
-    await test.step('Edit BP', async () => {
-      await frame.getByRole('button', { name: 'Edit blueprint' }).click();
-      await frame.getByTestId('revisit-hostname').click();
-      await frame.getByRole('textbox', { name: 'hostname input' }).click();
-      await frame
-        .getByRole('textbox', { name: 'hostname input' })
-        .fill(hostname + 'edited');
-      await frame.getByRole('button', { name: 'Review and finish' }).click();
-      await frame
-        .getByRole('button', { name: 'Save changes to blueprint' })
-        .click();
-    });
-
-    // This is for hosted service only as these features are not avaiable in cockpit plugin
-    if (isHosted()) {
-      await test.step('Export BP', async () => {
-        await exportBlueprint(frame);
-      });
-
-      await test.step('Import BP', async () => {
-        await importBlueprint(frame);
-      });
-
-      await test.step('Review imported BP', async () => {
-        await fillInImageOutputGuest(frame);
-        await frame.getByRole('button', { name: 'Hostname' }).click();
-        await expect(
-          frame.getByRole('textbox', { name: 'hostname input' })
-        ).toHaveValue(hostname + 'edited');
-        await frame.getByRole('button', { name: 'Cancel' }).click();
-      });
-    }
+  await test.step('Navigate to optional steps in Wizard', async () => {
+    await navigateToOptionalSteps(frame);
+    await registerLater(frame);
   });
+
+  await test.step('Select and fill the Hostname step', async () => {
+    if (isHosted()) {
+      await frame.getByRole('button', { name: 'Hostname' }).click();
+    } else {
+      await frame
+        .getByRole('listitem')
+        .filter({ hasText: /^Hostname$/ })
+        .click();
+    }
+    await frame.getByRole('textbox', { name: 'hostname input' }).click();
+    await frame.getByRole('textbox', { name: 'hostname input' }).fill(hostname);
+    await frame.getByRole('button', { name: 'Review and finish' }).click();
+  });
+
+  await test.step('Fill the BP details', async () => {
+    await fillInDetails(frame, blueprintName);
+  });
+
+  await test.step('Create BP', async () => {
+    await createBlueprint(frame, blueprintName);
+  });
+
+  await test.step('Edit BP', async () => {
+    await frame.getByRole('button', { name: 'Edit blueprint' }).click();
+    await frame.getByTestId('revisit-hostname').click();
+    await frame.getByRole('textbox', { name: 'hostname input' }).click();
+    await frame
+      .getByRole('textbox', { name: 'hostname input' })
+      .fill(hostname + 'edited');
+    await frame.getByRole('button', { name: 'Review and finish' }).click();
+    await frame
+      .getByRole('button', { name: 'Save changes to blueprint' })
+      .click();
+  });
+
+  // This is for hosted service only as these features are not avaiable in cockpit plugin
+  if (isHosted()) {
+    await test.step('Export BP', async () => {
+      await exportBlueprint(frame);
+    });
+
+    await test.step('Import BP', async () => {
+      await importBlueprint(frame);
+    });
+
+    await test.step('Review imported BP', async () => {
+      await fillInImageOutputGuest(frame);
+      await frame.getByRole('button', { name: 'Hostname' }).click();
+      await expect(
+        frame.getByRole('textbox', { name: 'hostname input' })
+      ).toHaveValue(hostname + 'edited');
+      await frame.getByRole('button', { name: 'Cancel' }).click();
+    });
+  }
 });
