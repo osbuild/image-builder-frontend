d12ff5885a010d8f1fca3533323e30859bf7d1c1
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-04-14 16:22:49 +0000

    cloudConfigSlice: add new slice

--- src/Components/CreateImageWizard/CreateImageWizard.tsx
+++ src/Components/CreateImageWizard/CreateImageWizard.tsx
@@ -374,7 +374,7 @@ const CreateImageWizard = ({ isEdit }: CreateImageWizardProps) => {
             id="step-target-environment"
             isHidden={
               !targetEnvironments.find(
-                (target) =>
+                (target: string) =>
                   target === 'aws' || target === 'gcp' || target === 'azure'
               )
             }
