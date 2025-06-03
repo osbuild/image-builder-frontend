1d81a287f0c1ac73b0e97e919f34583a396e0417
Simon Steinbeiss <simon.steinbeiss@redhat.com>
2025-06-03 18:34:01 +0200

    Drop Edge Management federated module (HMS-8637)

--- src/test/Components/edge/ImageDetails.test.tsx
+++
@@ -1,8 +0,0 @@
-import { useFlag } from '../../../Utilities/useGetEnvironment';
-
-describe('mocking unleash calls', () => {
-  test('the ege local image table is set to true', () => {
-    const edgeLocalImageTable = useFlag('image-builder.edge.local-image-table');
-    expect(edgeLocalImageTable).toBe(true);
-  });
-});
