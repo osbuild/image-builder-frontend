eb07309e513ad12702819acc72c20011d9b3e721
regexowl <ksimicko@redhat.com>
2025-07-03 10:19:09 +0200

    Run actions with Node 22
    The github actions were previously ran with Node 20, this bumps them to Node 22.
    
    README was also updated to reflect currently used version of Node.

--- build_deploy.sh
+++ build_deploy.sh
@@ -9,7 +9,7 @@ export COMPONENT="image-builder"
 export IMAGE="quay.io/cloudservices/image-builder-frontend"
 export APP_ROOT=$(pwd)
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
-export NODE_BUILD_VERSION=18
+export NODE_BUILD_VERSION=22
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
 set -exv
