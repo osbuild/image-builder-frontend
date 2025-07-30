d5321bb078466aa95b4f53cd3b2e2d559f0f52ea
regexowl <ksimicko@redhat.com>
2025-07-30 08:51:10 +0200

    Wizard: Remove alert from OpenSCAP step
    Information about required packages, services and kernel arguments is back on the step. Meaning we can remove the alert as per UX recommendation.

--- src/Components/CreateImageWizard/steps/Oscap/Oscap.tsx
+++ src/Components/CreateImageWizard/steps/Oscap/Oscap.tsx
@@ -42,18 +42,6 @@ const Oscap = ({ majorVersion }: OscapPropTypes) => {
       )}
       {complianceType === 'openscap' ? <ProfileSelector /> : <PolicySelector />}
       {oscapProfile && <OscapProfileInformation />}
-      {oscapProfile && (
-        <Alert
-          variant="info"
-          isInline
-          isPlain
-          title="Additional customizations"
-        >
-          Selecting an OpenSCAP profile will cause the appropriate packages,
-          file system configuration, kernel arguments, and services to be added
-          to your image.
-        </Alert>
-      )}
     </>
   );
 };
