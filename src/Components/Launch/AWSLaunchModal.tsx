281cb68ed132796e0b0c346c6e25db9a09386f4b
Anna Vítová <avitova@redhat.com>
2025-09-17 12:35:41 +0200

    Wizard: remove decommissiong flag for AWS (HMS-9017)
    This commit removes the decommissioning flag for AWS, and uses the new
    AWS launch modal.

--- src/Components/Launch/AWSLaunchModal.tsx
+++ src/Components/Launch/AWSLaunchModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 import {
   Button,
@@ -11,94 +11,114 @@ import {
   ModalHeader,
   ModalVariant,
   OrderType,
+  Skeleton,
 } from '@patternfly/react-core';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 import {
   ComposesResponseItem,
-  ComposeStatus,
+  useGetComposeStatusQuery,
 } from '../../store/imageBuilderApi';
 import { isAwsUploadRequestOptions } from '../../store/typeGuards';
 
 type LaunchProps = {
-  isOpen: boolean;
-  handleModalToggle: (event: KeyboardEvent | React.MouseEvent) => void;
   compose: ComposesResponseItem;
-  composeStatus: ComposeStatus | undefined;
 };
 
-export const AWSLaunchModal = ({
-  isOpen,
-  handleModalToggle,
-  compose,
-  composeStatus,
-}: LaunchProps) => {
-  const options = compose.request.image_requests[0].upload_request.options;
+export const AWSLaunchModal = ({ compose }: LaunchProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data, isSuccess, isFetching } = useGetComposeStatusQuery({
+    composeId: compose.id,
+  });
 
-  if (!isAwsUploadRequestOptions(options)) {
+  if (!isSuccess) {
+    return <Skeleton />;
+  }
+
+  const options = data?.image_status.upload_status?.options;
+
+  if (options && !isAwsUploadRequestOptions(options)) {
     throw TypeError(
       `Error: options must be of type AwsUploadRequestOptions, not ${typeof options}.`,
     );
   }
 
   const amiId =
-    composeStatus?.image_status.status === 'success' &&
-    composeStatus.image_status.upload_status?.options &&
-    'ami' in composeStatus.image_status.upload_status.options
-      ? composeStatus.image_status.upload_status.options.ami
+    data?.image_status.status === 'success' &&
+    data.image_status.upload_status?.options &&
+    'ami' in data.image_status.upload_status.options
+      ? data.image_status.upload_status.options.ami
       : '';
 
+  const handleModalToggle = (_event: KeyboardEvent | React.MouseEvent) => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={handleModalToggle}
-      variant={ModalVariant.large}
-      aria-label='Open launch guide modal'
-    >
-      <ModalHeader
-        title={'Launch with Amazon Web Services'}
-        labelId='modal-title'
-        description={compose.image_name}
-      />
-      <ModalBody id='modal-box-body-basic'>
-        <List component={ListComponent.ol} type={OrderType.number}>
-          <ListItem>
-            Navigate to the{' '}
-            <Button
-              component='a'
-              target='_blank'
-              variant='link'
-              icon={<ExternalLinkAltIcon />}
-              iconPosition='right'
-              href={`https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#ImageDetails:imageId=${amiId}`}
-              className='pf-v6-u-pl-0'
-            >
-              Images detail page
-            </Button>{' '}
-            located on your AWS console.
-          </ListItem>
-          <ListItem>
-            Copy the image to make it a permanent copy in your account.
-            <br />
-            Shared with Account{' '}
-            <span className='pf-v6-u-font-weight-bold'>
-              {options.share_with_accounts?.[0]}
-            </span>
-            <br />
-            AMI ID: <span className='pf-v6-u-font-weight-bold'>{amiId}</span>
-          </ListItem>
-          <ListItem>Launch image as an instance.</ListItem>
-          <ListItem>
-            Connect to it via SSH using the following username:{' '}
-            <span className='pf-v6-u-font-weight-bold'>ec2-user</span>
-          </ListItem>
-        </List>
-      </ModalBody>
-      <ModalFooter>
-        <Button key='close' variant='primary' onClick={handleModalToggle}>
-          Close
-        </Button>
-      </ModalFooter>
-    </Modal>
+    <Fragment>
+      <Button
+        variant='link'
+        isInline
+        isDisabled={data?.image_status.status !== 'success'}
+        onClick={handleModalToggle}
+      >
+        Launch
+      </Button>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleModalToggle}
+        variant={ModalVariant.large}
+        aria-label='Open launch guide modal'
+      >
+        <ModalHeader
+          title={'Launch with Amazon Web Services'}
+          labelId='modal-title'
+          description={compose.image_name}
+        />
+        <ModalBody id='modal-box-body-basic'>
+          <List component={ListComponent.ol} type={OrderType.number}>
+            <ListItem>
+              Navigate to the{' '}
+              <Button
+                component='a'
+                target='_blank'
+                variant='link'
+                icon={<ExternalLinkAltIcon />}
+                iconPosition='right'
+                href={`https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#ImageDetails:imageId=${amiId}`}
+                className='pf-v6-u-pl-0'
+              >
+                Images detail page
+              </Button>{' '}
+              located on your AWS console.
+            </ListItem>
+            <ListItem>
+              Copy the image to make it a permanent copy in your account.
+              <br />
+              Shared with Account{' '}
+              {!isFetching && (
+                <span className='pf-v6-u-font-weight-bold'>
+                  {options?.share_with_accounts?.[0]}
+                </span>
+              )}
+              {isFetching && <Skeleton />}
+              <br />
+              AMI ID: <span className='pf-v6-u-font-weight-bold'>{amiId}</span>
+            </ListItem>
+            <ListItem>Launch image as an instance.</ListItem>
+            <ListItem>
+              Connect to it via SSH using the following username:{' '}
+              <span className='pf-v6-u-font-weight-bold'>ec2-user</span>
+            </ListItem>
+          </List>
+        </ModalBody>
+        <ModalFooter>
+          <Button key='close' variant='primary' onClick={handleModalToggle}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </Fragment>
   );
 };
