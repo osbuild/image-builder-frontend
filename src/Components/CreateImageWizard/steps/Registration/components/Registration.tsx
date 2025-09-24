4183fa50490874d24e6580b28987bace9dbff372
regexowl <ksimicko@redhat.com>
2025-09-24 09:42:06 +0200

    Wizard: Update spacing on the Register step
    Spacing should be now mostly 1:1 with the final mocks.
    
    Previously used `component='p'` had to be replaced in several places - even thought resulting spacing looked correct there were several warnings outputted in the tests regarding DOM validation as neither <h4> (title of alert rendered when fetching activation keys) nor <div> (clipboard copy with org id) can be descendants of <p>.

--- src/Components/CreateImageWizard/steps/Registration/components/Registration.tsx
+++ src/Components/CreateImageWizard/steps/Registration/components/Registration.tsx
@@ -17,88 +17,96 @@ const Registration = () => {
 
   return (
     <FormGroup label='Registration method'>
-      <Radio
-        label='Automatically register and enable advanced capabilities.'
-        isChecked={
-          registrationType === 'register-now' ||
-          registrationType === 'register-now-insights' ||
-          registrationType === 'register-now-rhc'
-        }
-        onChange={(_event, checked) => {
-          if (checked) {
-            dispatch(changeRegistrationType('register-now-rhc'));
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          label='Automatically register and enable advanced capabilities.'
+          isChecked={
+            registrationType === 'register-now' ||
+            registrationType === 'register-now-insights' ||
+            registrationType === 'register-now-rhc'
           }
-        }}
-        id='register-system-now'
-        name='register-system-now'
-        autoFocus
-        body={
-          <>
-            <Content component='p'>
-              <Switch
-                label='Enable predictive analytics and management capabilities'
-                isChecked={
-                  registrationType === 'register-now-insights' ||
-                  registrationType === 'register-now-rhc'
-                }
-                onChange={(_event, checked) => {
-                  if (checked) {
-                    dispatch(changeRegistrationType('register-now-insights'));
-                  } else {
-                    dispatch(changeRegistrationType('register-now'));
+          onChange={(_event, checked) => {
+            if (checked) {
+              dispatch(changeRegistrationType('register-now-rhc'));
+            }
+          }}
+          id='register-system-now'
+          name='register-system-now'
+          autoFocus
+          body={
+            <>
+              <Content className='pf-v6-u-pb-sm'>
+                <Switch
+                  label='Enable predictive analytics and management capabilities'
+                  isChecked={
+                    registrationType === 'register-now-insights' ||
+                    registrationType === 'register-now-rhc'
                   }
-                }}
-                id='register-system-now-insights'
-                name='register-system-insights'
-                hasCheckIcon
-              />
-            </Content>
-            <Content component='p'>
-              <Switch
-                label='Enable remote remediations and system management with automation'
-                isChecked={registrationType === 'register-now-rhc'}
-                onChange={(_event, checked) => {
-                  if (checked) {
-                    dispatch(changeRegistrationType('register-now-rhc'));
-                  } else {
-                    dispatch(changeRegistrationType('register-now-insights'));
-                  }
-                }}
-                id='register-system-now-rhc'
-                name='register-system-rhc'
-                hasCheckIcon
-              />
-            </Content>
-            {!process.env.IS_ON_PREMISE &&
-              registrationType.startsWith('register-now') && (
-                <Content component='p'>
-                  <ActivationKeysList />
-                </Content>
-              )}
-          </>
-        }
-      />
-      <Radio
-        label='Register later'
-        isChecked={registrationType === 'register-later'}
-        onChange={() => {
-          dispatch(changeRegistrationType('register-later'));
-        }}
-        id='register-later'
-        name='register-later'
-      />
-      <Radio
-        label='Register with Satellite'
-        isChecked={registrationType === 'register-satellite'}
-        onChange={() => {
-          dispatch(changeRegistrationType('register-satellite'));
-        }}
-        id='register-satellite'
-        name='register-satellite'
-        body={
-          registrationType === 'register-satellite' && <SatelliteRegistration />
-        }
-      />
+                  onChange={(_event, checked) => {
+                    if (checked) {
+                      dispatch(changeRegistrationType('register-now-insights'));
+                    } else {
+                      dispatch(changeRegistrationType('register-now'));
+                    }
+                  }}
+                  id='register-system-now-insights'
+                  name='register-system-insights'
+                  hasCheckIcon
+                />
+              </Content>
+              <Content className='pf-v6-u-pb-sm'>
+                <Switch
+                  label='Enable remote remediations and system management with automation'
+                  isChecked={registrationType === 'register-now-rhc'}
+                  onChange={(_event, checked) => {
+                    if (checked) {
+                      dispatch(changeRegistrationType('register-now-rhc'));
+                    } else {
+                      dispatch(changeRegistrationType('register-now-insights'));
+                    }
+                  }}
+                  id='register-system-now-rhc'
+                  name='register-system-rhc'
+                  hasCheckIcon
+                />
+              </Content>
+              {!process.env.IS_ON_PREMISE &&
+                registrationType.startsWith('register-now') && (
+                  <Content className='pf-v6-u-pb-sm'>
+                    <ActivationKeysList />
+                  </Content>
+                )}
+            </>
+          }
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          label='Register later'
+          isChecked={registrationType === 'register-later'}
+          onChange={() => {
+            dispatch(changeRegistrationType('register-later'));
+          }}
+          id='register-later'
+          name='register-later'
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          label='Register with Satellite'
+          isChecked={registrationType === 'register-satellite'}
+          onChange={() => {
+            dispatch(changeRegistrationType('register-satellite'));
+          }}
+          id='register-satellite'
+          name='register-satellite'
+          body={
+            registrationType === 'register-satellite' && (
+              <SatelliteRegistration />
+            )
+          }
+        />
+      </Content>
     </FormGroup>
   );
 };
