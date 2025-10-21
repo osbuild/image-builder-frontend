d641d16f32a678cf4961c8dd813c42358c88fdca
regexowl <ksimicko@redhat.com>
2025-10-21 10:18:34 +0200

    devDeps: Bump playwright and @playwright/test
    This bumps playwright to 1.56.1 and updates ancestor dependency @playwright/test. These dependencies need to be updated together.

--- schutzbot/playwright_tests.sh
+++ schutzbot/playwright_tests.sh
@@ -86,5 +86,5 @@ sudo podman run \
      --privileged  \
      --rm \
      --init \
-     mcr.microsoft.com/playwright:v1.51.1-noble \
-     /bin/sh -c "cd tests && npx -y playwright@1.51.1 test --workers=${PW_WORKERS}"
+     mcr.microsoft.com/playwright:v1.56.1-noble \
+     /bin/sh -c "cd tests && npx -y playwright@1.56.1 test --workers=${PW_WORKERS}"
