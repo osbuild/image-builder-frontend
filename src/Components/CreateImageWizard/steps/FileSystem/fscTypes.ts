8003b1e21903d6be861885481a160ec505fcbde3
regexowl <ksimicko@redhat.com>
2025-10-23 09:29:58 +0200

    Wizard: Add editable disk partition tables
    This adds plain disk partition table and an option to add a volume group. Volume groups are added as a card that includes a file system table of logical volumes.

--- src/Components/CreateImageWizard/steps/FileSystem/fscTypes.ts
+++ src/Components/CreateImageWizard/steps/FileSystem/fscTypes.ts
@@ -1,3 +1,10 @@
+import {
+  BtrfsVolume,
+  FilesystemTyped,
+  LogicalVolume,
+  Minsize,
+} from '../../../../store/imageBuilderApi';
+
 export type FilesystemPartition = {
   id: string;
   mountpoint: string;
@@ -6,3 +13,32 @@ export type FilesystemPartition = {
 };
 
 export type Units = 'B' | 'KiB' | 'MiB' | 'GiB';
+
+export type FSType = 'ext4' | 'xfs' | 'vfat' | 'swap';
+
+export type FscDisk = {
+  minsize: string;
+  partitions: FscDiskPartition[];
+  type?: 'gpt' | 'dos' | undefined;
+};
+
+export type FscDiskPartitionBase = {
+  id: string;
+  min_size: string | undefined;
+  unit: Units | undefined;
+};
+
+export type LogicalVolumeWithBase = LogicalVolume & FscDiskPartitionBase;
+
+export type VolumeGroupWithExtendedLV = FscDiskPartitionBase & {
+  type: 'lvm';
+  part_type?: string | undefined;
+  name?: string | undefined;
+  minsize?: Minsize | undefined;
+  logical_volumes: LogicalVolumeWithBase[];
+};
+
+export type FscDiskPartition =
+  | (FilesystemTyped & FscDiskPartitionBase)
+  | VolumeGroupWithExtendedLV
+  | (BtrfsVolume & FscDiskPartitionBase);
