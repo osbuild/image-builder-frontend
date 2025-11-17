f967ef42ca09000eb3951e44064918726ca08759
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-11-17 09:58:48 +0100

    api: stop updating provisioning
    The api itself is no longer available, as it is being decommissioned.

--- api/codegen.sh
+++ api/codegen.sh
@@ -4,7 +4,6 @@
 npx @rtk-query/codegen-openapi ./api/config/imageBuilder.ts &
 npx @rtk-query/codegen-openapi ./api/config/rhsm.ts &
 npx @rtk-query/codegen-openapi ./api/config/contentSources.ts &
-npx @rtk-query/codegen-openapi ./api/config/provisioning.ts &
 npx @rtk-query/codegen-openapi ./api/config/compliance.ts &
 npx @rtk-query/codegen-openapi ./api/config/composerCloudApi.ts &
 
