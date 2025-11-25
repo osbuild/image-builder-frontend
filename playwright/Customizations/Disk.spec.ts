6b3a2ca7572d41ad2682784ebe6808d8c3244ebd
regexowl <ksimicko@redhat.com>
2025-11-25 12:04:26 +0100

    src: Add root to plain when no root present
    This adds root partition on "Add plain partition" button when no root is present in the configuration.

--- playwright/Customizations/Disk.spec.ts
+++ playwright/Customizations/Disk.spec.ts
@@ -59,7 +59,7 @@ test('Create a blueprint with Disk customization', async ({
     await expect(frame.getByRole('button', { name: '/' })).toBeDisabled();
     await expect(
       frame.getByRole('row').nth(1).getByRole('button').nth(4),
-    ).toBeEnabled();
+    ).toBeDisabled();
 
     await expect(
       frame.getByRole('button', {
@@ -97,6 +97,9 @@ test('Create a blueprint with Disk customization', async ({
     await expect(removeRootButton).toBeDisabled();
 
     await frame.getByRole('button', { name: 'Add LVM volume group' }).click();
+    await expect(
+      frame.getByRole('row').nth(1).getByRole('button').nth(4),
+    ).toBeEnabled();
 
     await frame
       .getByRole('textbox', { name: 'Volume group name input' })
@@ -155,7 +158,7 @@ test('Create a blueprint with Disk customization', async ({
       .nth(1)
       .getByRole('button')
       .nth(4);
-    await expect(removeRootButton).toBeDisabled();
+    await expect(removeRootButton).toBeEnabled();
 
     const secondRow = frame.getByRole('row').nth(2);
 
@@ -244,7 +247,7 @@ test('Create a blueprint with Disk customization', async ({
       .getByRole('button')
       .nth(4);
 
-    await expect(removeRootButton).toBeDisabled();
+    await expect(removeRootButton).toBeEnabled();
 
     const secondRow = frame.getByRole('row').nth(2);
 
