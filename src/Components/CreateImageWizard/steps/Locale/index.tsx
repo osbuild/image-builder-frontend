f676c284ccd15a7d2b1dbf8bb5ef001d5aa384fd
Katarina Sieklova <ksieklov@redhat.com>
2025-12-12 15:17:34 +0100

    Wizard: add langpack if a locale is selected
    Adds langpack based on a user's locale selection. If the user removes
    the langpack from "Additional packages", the langpack gets suppressed in the Locale
    step to avoid adding it again.

--- src/Components/CreateImageWizard/steps/Locale/index.tsx
+++ src/Components/CreateImageWizard/steps/Locale/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Content, Form, Title } from '@patternfly/react-core';
+import { Content, Form, Spinner, Title } from '@patternfly/react-core';
+
+import { useSearchLanguagePacks } from '@/store/api/distributions';
 
 import KeyboardDropDown from './components/KeyboardDropDown';
 import LanguagesDropDown from './components/LanguagesDropDown';
 
+import { useGetArchitecturesQuery } from '../../../../store/backendApi';
+import { useAppSelector } from '../../../../store/hooks';
+import { asDistribution } from '../../../../store/typeGuards';
+import {
+  selectArchitecture,
+  selectDistribution,
+  selectLocaleLangpackCandidates,
+  selectVerifiedLocaleLangpacks,
+} from '../../../../store/wizardSlice';
 import { CustomizationLabels } from '../../../sharedComponents/CustomizationLabels';
 
 const LocaleStep = () => {
+  const distribution = useAppSelector(selectDistribution);
+  const arch = useAppSelector(selectArchitecture);
+  const candidateLangpacks = useAppSelector(selectLocaleLangpackCandidates);
+  const { data: distroRepositories, isLoading: isArchitecturesLoading } =
+    useGetArchitecturesQuery({
+      distribution: asDistribution(distribution),
+    });
+
+  const distroUrls = useMemo(() => {
+    return (
+      distroRepositories
+        ?.find((archItem) => archItem.arch === arch)
+        ?.repositories.filter((repo) => !!repo.baseurl)
+        .map((repo) => repo.baseurl!) ?? []
+    );
+  }, [distroRepositories, arch]);
+
+  const { isLoading: isSearchLoading } = useSearchLanguagePacks(distroUrls);
+  const verifiedLangpacks = useAppSelector(selectVerifiedLocaleLangpacks);
+
+  const isLoading =
+    candidateLangpacks.length > 0 &&
+    (isArchitecturesLoading || isSearchLoading);
+
   return (
     <Form>
       <CustomizationLabels customization='locale' />
@@ -16,6 +51,23 @@ const LocaleStep = () => {
       </Title>
       <Content>Select the locale for your image.</Content>
       <LanguagesDropDown />
+      {isLoading && (
+        <Content>
+          <Spinner size='md' /> Resolving packages for your preferred locale…
+        </Content>
+      )}
+      {!isLoading && verifiedLangpacks.length > 0 && (
+        <Content>
+          The following packages will be added based on your preferred locale:{' '}
+          {verifiedLangpacks.map((pkg, idx) => (
+            <strong key={pkg}>
+              {pkg}
+              {idx < verifiedLangpacks.length - 1 ? ', ' : ''}
+            </strong>
+          ))}
+          .
+        </Content>
+      )}
       <KeyboardDropDown />
     </Form>
   );
