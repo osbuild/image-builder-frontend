886d1d31c0d33de4751227554cb3be47713c3630
regexowl <ksimicko@redhat.com>
2025-12-16 15:08:52 +0100

    LandingPage: Update "Service unavailable" alert with a payload
    This allows to render more information about outage via parsing an unleash payload.

--- src/Components/LandingPage/ServiceUnavailableAlert.tsx
+++ src/Components/LandingPage/ServiceUnavailableAlert.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 import { Alert } from '@patternfly/react-core';
+import { useVariant } from '@unleash/proxy-client-react';
 
 const ServiceUnavailableAlert = () => {
+  const variant = useVariant('image-builder.service-unavailable');
+  const payload = variant.payload
+    ? JSON.parse(variant.payload.value)
+    : undefined;
+
+  const title =
+    payload?.title ||
+    'The Image Builder service is currently unavailable. Please check back later.';
+  const body = payload?.body || '';
+
   return (
-    <Alert
-      title='The Image Builder service is currently unavailable. Please check back later.'
-      variant='danger'
-    />
+    <Alert title={title} variant='danger'>
+      {body}
+      {body && (
+        <>
+          <br />
+          We&apos;ve identified the issue and are working on a fix.
+        </>
+      )}
+    </Alert>
   );
 };
 
