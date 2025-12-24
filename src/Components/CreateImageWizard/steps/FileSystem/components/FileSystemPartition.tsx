75ca047207f1476e35a726f5b040d5df3fb07cf2
regexowl <ksimicko@redhat.com>
2025-12-24 07:50:50 +0100

    Wizard: Space out FSC options
    This adds spacing to the automatic/basic/advanced partitioning radio buttons on the FSC step.

--- src/Components/CreateImageWizard/steps/FileSystem/components/FileSystemPartition.tsx
+++ src/Components/CreateImageWizard/steps/FileSystem/components/FileSystemPartition.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FormGroup, Label, Radio } from '@patternfly/react-core';
+import { Content, FormGroup, Radio } from '@patternfly/react-core';
 
 import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
 import {
@@ -20,43 +20,42 @@ const FileSystemPartition = () => {
 
   return (
     <FormGroup>
-      <Radio
-        id='automatic file system config radio'
-        label={
-          <>
-            <Label isCompact color='blue'>
-              Recommended
-            </Label>{' '}
-            Use automatic partitioning
-          </>
-        }
-        name='fsc-type'
-        description='Automatically partition your image to what is best, depending on the target environment(s)'
-        isChecked={fscMode === 'automatic'}
-        onChange={() => {
-          dispatch(changeFscMode('automatic'));
-        }}
-      />
-      <Radio
-        id='basic-partitioning-radio'
-        label='Basic filesystem partitioning'
-        name='fsc-type'
-        description='Configure the file system of your image by adding, removing, and editing partitions'
-        isChecked={fscMode === 'basic'}
-        onChange={() => {
-          dispatch(changeFscMode('basic'));
-        }}
-      />
-      <Radio
-        id='advanced-partitioning-radio'
-        label='Advanced disk partitioning'
-        name='fsc-type'
-        description='Configure disk partitioning with advanced options'
-        isChecked={fscMode === 'advanced'}
-        onChange={() => {
-          dispatch(changeFscMode('advanced'));
-        }}
-      />
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='automatic file system config radio'
+          label='Use automatic partitioning'
+          name='fsc-type'
+          description='Automatically partition your image to what is best, depending on the target environment(s)'
+          isChecked={fscMode === 'automatic'}
+          onChange={() => {
+            dispatch(changeFscMode('automatic'));
+          }}
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='basic-partitioning-radio'
+          label='Basic filesystem partitioning'
+          name='fsc-type'
+          description='Configure the file system of your image by adding, removing, and editing partitions'
+          isChecked={fscMode === 'basic'}
+          onChange={() => {
+            dispatch(changeFscMode('basic'));
+          }}
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='advanced-partitioning-radio'
+          label='Advanced disk partitioning'
+          name='fsc-type'
+          description='Configure disk partitioning with advanced options'
+          isChecked={fscMode === 'advanced'}
+          onChange={() => {
+            dispatch(changeFscMode('advanced'));
+          }}
+        />
+      </Content>
     </FormGroup>
   );
 };
