625fc973b0ad2709c22307115356061964222ae3
regexowl <ksimicko@redhat.com>
2025-12-24 07:51:41 +0100

    Wizard: Space out partitioning mode options
    This updates spacing for auto-lvm/raw/lvm partitioning radios under the partitioning mode checkbox.

--- src/Components/CreateImageWizard/steps/FileSystem/components/PartitioningMode.tsx
+++ src/Components/CreateImageWizard/steps/FileSystem/components/PartitioningMode.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Radio } from '@patternfly/react-core';
+import { Content, Radio } from '@patternfly/react-core';
 
 import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
 import {
@@ -14,36 +14,42 @@ const PartitioningMode = () => {
 
   return (
     <>
-      <Radio
-        id='auto-lvm-partitioning-mode-radio'
-        label='Auto-LVM partitioning'
-        name='partitioning-mode'
-        description='Converts partitions to LVM only if new mountpoints are defined in the filesystem customization'
-        isChecked={partitioningMode === 'auto-lvm'}
-        onChange={() => {
-          dispatch(changePartitioningMode('auto-lvm'));
-        }}
-      />
-      <Radio
-        id='raw-partitioning-mode-radio'
-        label='Raw partitioning'
-        name='partitioning-mode'
-        description='Will not convert any partition to LVM or Btrfs'
-        isChecked={partitioningMode === 'raw'}
-        onChange={() => {
-          dispatch(changePartitioningMode('raw'));
-        }}
-      />
-      <Radio
-        id='lvm-partitioning-mode-radio'
-        label='LVM partitioning'
-        name='partitioning-mode'
-        description='Converts the partition that contains the root mountpoint / to an LVM Volume Group and creates a root Logical Volume. Any extra mountpoints, except /boot, will be added to the Volume Group as new Logical Volumes'
-        isChecked={partitioningMode === 'lvm'}
-        onChange={() => {
-          dispatch(changePartitioningMode('lvm'));
-        }}
-      />
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='auto-lvm-partitioning-mode-radio'
+          label='Auto-LVM partitioning'
+          name='partitioning-mode'
+          description='Converts partitions to LVM only if new mountpoints are defined in the filesystem customization'
+          isChecked={partitioningMode === 'auto-lvm'}
+          onChange={() => {
+            dispatch(changePartitioningMode('auto-lvm'));
+          }}
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='raw-partitioning-mode-radio'
+          label='Raw partitioning'
+          name='partitioning-mode'
+          description='Will not convert any partition to LVM or Btrfs'
+          isChecked={partitioningMode === 'raw'}
+          onChange={() => {
+            dispatch(changePartitioningMode('raw'));
+          }}
+        />
+      </Content>
+      <Content className='pf-v6-u-pb-sm'>
+        <Radio
+          id='lvm-partitioning-mode-radio'
+          label='LVM partitioning'
+          name='partitioning-mode'
+          description='Converts the partition that contains the root mountpoint / to an LVM Volume Group and creates a root Logical Volume. Any extra mountpoints, except /boot, will be added to the Volume Group as new Logical Volumes'
+          isChecked={partitioningMode === 'lvm'}
+          onChange={() => {
+            dispatch(changePartitioningMode('lvm'));
+          }}
+        />
+      </Content>
     </>
   );
 };
