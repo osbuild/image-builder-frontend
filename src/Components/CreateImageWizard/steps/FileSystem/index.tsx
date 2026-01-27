2dd87b4fffaec6c9a760e886b764b5015238836b
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-01-27 14:24:56 +0000

    Wizard/FSC: remove hasIsoTargetOnly check
    We hide this step now if the user only selects the iso target,
    so this check is redundant.

--- src/Components/CreateImageWizard/steps/FileSystem/index.tsx
+++ src/Components/CreateImageWizard/steps/FileSystem/index.tsx
@@ -9,13 +9,11 @@ import FileSystemPartition from './components/FileSystemPartition';
 
 import { useAppSelector } from '../../../../store/hooks';
 import { selectFscMode } from '../../../../store/wizardSlice';
-import { useHasSpecificTargetOnly } from '../../utilities/hasSpecificTargetOnly';
 
 export type FscModeType = 'automatic' | 'basic' | 'advanced';
 
 const FileSystemStep = () => {
   const fscMode = useAppSelector(selectFscMode);
-  const hasIsoTargetOnly = useHasSpecificTargetOnly('image-installer');
 
   return (
     <Form>
@@ -23,9 +21,7 @@ const FileSystemStep = () => {
         File system configuration
       </Title>
       <Content>Define the partitioning of the image.</Content>
-      {hasIsoTargetOnly ? (
-        <FileSystemAutomaticPartition />
-      ) : fscMode === 'automatic' ? (
+      {fscMode === 'automatic' ? (
         <>
           <FileSystemPartition />
           <FileSystemAutomaticPartition />
