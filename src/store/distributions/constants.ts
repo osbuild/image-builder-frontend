abfdde87a6dd502be8828c13c4a0b406e367d406
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-01-27 11:52:34 +0000

    store/distributions: create an endpoint for customizations
    This is a small dummy endpoint that simulates querying the backend
    to get the list of allowed customization types based on the image
    type selection.

---
+++ src/store/distributions/constants.ts
@@ -0,0 +1,71 @@
+import { CustomizationType, ImageTypeInfo } from './types';
+
+export const ALL_CUSTOMIZATIONS = [
+  'packages',
+  'repositories',
+  'filesystem',
+  'kernel',
+  'timezone',
+  'locale',
+  'firewall',
+  'services',
+  'hostname',
+  'firstBoot',
+  'openscap',
+  'users',
+  'fips',
+  'aap',
+] as const satisfies readonly CustomizationType[];
+
+// We are just mocking the response until we can actually
+// get this information from the API.
+export const DISTRO_DETAILS: Record<string, ImageTypeInfo> = {
+  aws: {
+    name: 'aws',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  azure: {
+    name: 'azure',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  vhd: {
+    name: 'vhd',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  gcp: { name: 'gcp', supported_blueprint_options: [...ALL_CUSTOMIZATIONS] },
+  'guest-image': {
+    name: 'guest-image',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  'image-installer': {
+    name: 'image-installer',
+    supported_blueprint_options: [
+      // image-installer has everything but filesystem
+      ...ALL_CUSTOMIZATIONS.filter((c) => c !== 'filesystem'),
+    ],
+  },
+  vsphere: {
+    name: 'vsphere',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  'vsphere-ova': {
+    name: 'vsphere-ova',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  wsl: {
+    name: 'wsl',
+    supported_blueprint_options: [
+      // wsl has everything but filesystem & kernel
+      ...ALL_CUSTOMIZATIONS.filter((c) => c !== 'filesystem' && c !== 'kernel'),
+    ],
+  },
+  ami: { name: 'ami', supported_blueprint_options: [...ALL_CUSTOMIZATIONS] },
+  oci: {
+    name: 'oci',
+    supported_blueprint_options: [...ALL_CUSTOMIZATIONS],
+  },
+  'network-installer': {
+    name: 'network-installer',
+    supported_blueprint_options: ['locale', 'fips'],
+  },
+};
