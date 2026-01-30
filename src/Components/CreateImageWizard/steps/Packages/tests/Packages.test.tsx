0a530e149cba89df94029abd6fb4cee4e9332efd
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-01-30 18:06:40 +0000

    test: mock the useDebounce hook for Packages
    There is no need to debounce here since these are unit tests.
    Doing this shortens the unit test runtime quite significantly.

--- src/Components/CreateImageWizard/steps/Packages/tests/Packages.test.tsx
+++ src/Components/CreateImageWizard/steps/Packages/tests/Packages.test.tsx
@@ -20,6 +20,29 @@ import { server } from '../../../../../test/mocks/server';
 
 fetchMock.enableMocks();
 
+vi.mock('../../../../../Utilities/useDebounce', async () => {
+  const { useState, useEffect } = await import('react');
+  const { isEqual } = await import('lodash');
+
+  return {
+    default: function useDebounce<T>(value: T, delay: number = 100): T {
+      const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+      useEffect(() => {
+        if (!isEqual(value, debouncedValue)) {
+          const timer = setTimeout(
+            () => setDebouncedValue(value),
+            value === '' ? 0 : delay,
+          );
+          return () => clearTimeout(timer);
+        }
+      }, [value, delay, debouncedValue]);
+
+      return debouncedValue;
+    },
+  };
+});
+
 // Disable global MSW server for this file - we use fetch mocks instead
 beforeAll(() => {
   server.close();
