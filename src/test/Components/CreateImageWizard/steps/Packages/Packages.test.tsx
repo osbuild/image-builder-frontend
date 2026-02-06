47c409e714ccf5a96395f30bebeaba7a4baffeb3
Michal Gold <mgold@redhat.com>
2026-02-06 14:42:36 +0200

    Wizard: Add conditional search instructions for Additional Packages step Show different search instructions based on environment (on-premise vs hosted) to guide users on the available search capabilities. Add test to check it in hosted env, will add test in onprem env in a follow up pr because it is out of the scope.

--- src/test/Components/CreateImageWizard/steps/Packages/Packages.test.tsx
+++ src/test/Components/CreateImageWizard/steps/Packages/Packages.test.tsx
@@ -232,7 +232,16 @@ describe('Step Packages', () => {
       name: /Search packages/i,
     });
     await screen.findByText(
-      /Search for exact matches by specifying the package name to add additional packages to your image/i,
+      /Search for additional packages to add to your image/i,
+    );
+  });
+
+  test('shows package group search instructions', async () => {
+    await renderCreateMode();
+    await goToPackagesStep();
+
+    await screen.findByText(
+      /Search for package groups by starting your search with the '@' character/i,
     );
   });
 
