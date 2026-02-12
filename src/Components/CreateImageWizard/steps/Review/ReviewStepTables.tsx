a07db7258d5db756c21671736ca9e2967023d140
regexowl <ksimicko@redhat.com>
2026-02-12 13:45:54 +0100

    Wizard: Add advance partitioning to review step
    This correctly changes configuration type to "Advanced" when advanced partitioning is selected and updated the partitions table to reflect the disk structure.

--- src/Components/CreateImageWizard/steps/Review/ReviewStepTables.tsx
+++ src/Components/CreateImageWizard/steps/Review/ReviewStepTables.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 
 import {
   Alert,
+  Content,
   EmptyState,
   Panel,
   PanelMain,
@@ -18,6 +19,7 @@ import {
 import { useAppSelector } from '../../../../store/hooks';
 import {
   selectCustomRepositories,
+  selectDiskPartitions,
   selectDistribution,
   selectFilesystemPartitions,
   selectGroups,
@@ -105,6 +107,79 @@ export const FSReviewTable = () => {
             ))}
           </Tbody>
         </Table>
+      </PanelMain>
+    </Panel>
+  );
+};
+
+export const DiskReviewTable = () => {
+  const partitions = useAppSelector(selectDiskPartitions);
+  return (
+    <Panel isScrollable>
+      <PanelMain>
+        <Content component='h4'>Plain partitions</Content>
+        <Table aria-label='Plain partitions' variant='compact'>
+          <Thead>
+            <Tr>
+              <Th>Mount point</Th>
+              <Th>File system type</Th>
+              <Th>Minimum size</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {partitions
+              .filter((p) => 'mountpoint' in p)
+              .map((partition) => (
+                <Tr key={partition.id}>
+                  <Td className='pf-m-width-30'>
+                    {'mountpoint' in partition ? partition.mountpoint : ''}
+                  </Td>
+                  <Td className='pf-m-width-30'>xfs</Td>
+                  <Td className='pf-m-width-30'>
+                    {partition.min_size
+                      ? `${partition.min_size} ${partition.unit}`
+                      : ''}
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+        {partitions
+          .filter((p) => 'logical_volumes' in p)
+          .map((vg) => {
+            return (
+              <React.Fragment key={vg.id}>
+                <Content component='h4'>
+                  Volume group {vg.name && vg.name}
+                </Content>
+                {vg.min_size && (
+                  <Content>Minimum size: {`${vg.min_size} ${vg.unit}`}</Content>
+                )}
+                <Table aria-label='Logical volumes' variant='compact'>
+                  <Thead>
+                    <Tr>
+                      <Th>Name</Th>
+                      <Th>Mount point</Th>
+                      <Th>File system type</Th>
+                      <Th>Minimum size</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {vg.logical_volumes.map((lv, lvIndex) => (
+                      <Tr key={lvIndex}>
+                        <Td>{lv.name}</Td>
+                        <Td>{lv.mountpoint}</Td>
+                        <Td>{lv.fs_type}</Td>
+                        <Td>
+                          {lv.min_size ? `${lv.min_size} ${lv.unit}` : ''}
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </React.Fragment>
+            );
+          })}
       </PanelMain>
     </Panel>
   );
