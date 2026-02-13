f24ace606ab1844651b1897df2b719b2f238b0ce
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-02-13 12:58:04 +0000

    store/distributions: refactor customizations hook
    Create a helper function to make it slightly more re-usable since we
    will need to use the same logic for returning an array of image types
    and whether or not they support a given customization.

--- src/store/distributions/hooks.tsx
+++ src/store/distributions/hooks.tsx
@@ -2,11 +2,7 @@ import { useMemo } from 'react';
 
 import { ALL_CUSTOMIZATIONS } from './constants';
 import { distroDetailsApi as api } from './distributionDetailsApi';
-import {
-  CustomizationType,
-  DistributionDetails,
-  RestrictionStrategy,
-} from './types';
+import { CustomizationType, ImageTypeInfo, RestrictionStrategy } from './types';
 
 import isRhel from '../../Utilities/isRhel';
 import { selectIsOnPremise } from '../envSlice';
@@ -18,20 +14,47 @@ import {
   selectIsImageMode,
 } from '../wizardSlice';
 
-export type ComputeRestrictionsArgs = {
+export type SupportContext = {
   isImageMode: boolean;
   isOnPremise: boolean;
-  arch: string;
-  distro: string;
-  data: DistributionDetails | undefined;
+  isRhel: boolean;
+};
+
+export const isCustomizationSupported = (
+  customization: CustomizationType,
+  imageType: ImageTypeInfo | undefined,
+  ctx: SupportContext,
+) => {
+  if (ctx.isImageMode) {
+    return ['filesystem', 'users'].includes(customization);
+  }
+
+  if (
+    ctx.isOnPremise &&
+    // on-premise doesn't allow first boot & repository
+    // customizations just yet
+    ['repositories', 'firstBoot'].includes(customization)
+  ) {
+    return false;
+  }
+
+  // only rhel distros support registration
+  if (!ctx.isRhel && customization === 'registration') {
+    return false;
+  }
+
+  const supportedOptions = imageType?.supported_blueprint_options;
+  return !supportedOptions || supportedOptions.includes(customization);
+};
+
+export type ComputeRestrictionsArgs = {
+  imageTypes: Record<string, ImageTypeInfo>;
+  context: SupportContext;
 };
 
 export const computeRestrictions = ({
-  isImageMode,
-  isOnPremise,
-  arch,
-  distro,
-  data,
+  imageTypes,
+  context: ctx,
 }: ComputeRestrictionsArgs): Record<CustomizationType, RestrictionStrategy> => {
   const result: Record<CustomizationType, RestrictionStrategy> = {} as Record<
     CustomizationType,
@@ -39,76 +62,25 @@ export const computeRestrictions = ({
   >;
 
   for (const customization of ALL_CUSTOMIZATIONS) {
-    if (isImageMode) {
-      // users & filesystem are the only allowed customization for image mode,
-      const allowed = ['filesystem', 'users'].includes(customization);
-      result[customization] = {
-        shouldHide: !allowed,
-        // users is required for image-mode
-        required: customization === 'users',
-      };
-      continue;
-    }
-
-    if (
-      isOnPremise &&
-      // on-premise doesn't allow first boot & repository
-      // customizations just yet
-      ['repositories', 'firstBoot'].includes(customization)
-    ) {
-      result[customization] = {
-        shouldHide: true,
-        required: false,
-      };
-      continue;
-    }
-
-    if (!isRhel(distro) && customization === 'registration') {
-      result[customization] = {
-        shouldHide: true,
-        required: false,
-      };
-      continue;
-    }
-
-    result[customization] = {
-      shouldHide: false,
-      required: false,
-    };
-
-    const architectures = data?.architectures;
-    if (!architectures) {
-      continue;
-    }
-
-    if (!Object.keys(architectures).includes(arch)) {
-      continue;
-    }
-
-    const imageTypes = architectures[arch].image_types;
-    // at this point the user probably doesn't have any
-    // image types selected, so let's return the default list
-    if (!imageTypes || Object.keys(imageTypes).length === 0) {
-      continue;
-    }
-
-    // we can collect a set of the supported image types, this way we can catch the
-    // case where a user selects multiple image types that all don't support a certain
-    // customization. For example, wsl & image-installer both don't support fs
-    // customizations, so it's safe to hide this option if the user selects both.
     const supportedOptions: Set<string> = new Set();
-    for (const imageType of Object.keys(imageTypes)) {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (!imageTypes[imageType]) continue;
 
-      imageTypes[imageType].supported_blueprint_options?.forEach((option) => {
-        supportedOptions.add(option);
-      });
+    // This covers the default case at the beginning of the wizard
+    // when the user hasn't selected any image types yet
+    if (Object.keys(imageTypes).length === 0) {
+      if (isCustomizationSupported(customization, undefined, ctx)) {
+        supportedOptions.add(customization);
+      }
+    }
+
+    for (const it of Object.keys(imageTypes)) {
+      if (isCustomizationSupported(customization, imageTypes[it], ctx)) {
+        supportedOptions.add(customization);
+      }
     }
 
     result[customization] = {
       shouldHide: !supportedOptions.has(customization),
-      required: false,
+      required: ctx.isImageMode && customization === 'users',
     };
   }
 
@@ -142,14 +114,27 @@ export const useCustomizationRestrictions = ({
   );
 
   const restrictions = useMemo(() => {
+    let imageTypes = {};
+    if (
+      data?.architectures &&
+      // eslint complains about this always being falsy, but there are cases
+      // where this does actually happen and can cause some rendering issues.
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      data.architectures[arch] &&
+      data.architectures[arch].image_types
+    ) {
+      imageTypes = data.architectures[arch].image_types;
+    }
+
     return computeRestrictions({
-      isImageMode,
-      isOnPremise,
-      arch,
-      distro,
-      data,
+      imageTypes,
+      context: {
+        isImageMode,
+        isOnPremise,
+        isRhel: isRhel(distro),
+      },
     });
-  }, [data, arch, distro, isImageMode, isOnPremise]);
+  }, [data, distro, arch, isImageMode, isOnPremise]);
 
   return {
     restrictions,
