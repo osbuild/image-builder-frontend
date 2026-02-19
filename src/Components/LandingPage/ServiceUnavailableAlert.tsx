b57aed804321354ecbe65f64e28dec20f8887053
Anna Vítová <avitova@redhat.com>
2026-02-19 15:06:17 +0100

    LandingPage: Update "Service unavailable" with option to set to warning.
    This commit enables an option to fill in a variant of the alert so that we are able to set the alert to warning as well.

--- src/Components/LandingPage/ServiceUnavailableAlert.tsx
+++ src/Components/LandingPage/ServiceUnavailableAlert.tsx
@@ -3,6 +3,17 @@ import React from 'react';
 import { Alert } from '@patternfly/react-core';
 import { useVariant } from '@unleash/proxy-client-react';
 
+const getValidAlertVariant = (variant: unknown): string => {
+  if (typeof variant === 'string') {
+    if (variant === 'success') return 'success';
+    if (variant === 'info') return 'info';
+    if (variant === 'warning') return 'warning';
+    if (variant === 'danger') return 'danger';
+    if (variant === 'custom') return 'custom';
+  }
+  return 'danger';
+};
+
 const ServiceUnavailableAlert = () => {
   const variant = useVariant('image-builder.service-unavailable');
   const payload = variant.payload
@@ -13,9 +24,15 @@ const ServiceUnavailableAlert = () => {
     payload?.title ||
     'The Image Builder service is currently unavailable. Please check back later.';
   const body = payload?.body || '';
+  const alertVariant = getValidAlertVariant(payload?.variant) || 'danger';
 
   return (
-    <Alert title={title} variant='danger'>
+    <Alert
+      title={title}
+      variant={
+        alertVariant as 'success' | 'info' | 'warning' | 'danger' | 'custom'
+      }
+    >
       {body}
       {body && (
         <>
