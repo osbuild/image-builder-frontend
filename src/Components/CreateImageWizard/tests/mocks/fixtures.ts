016b0615e540b766e15113a6dc1b1095c2405032
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-03-04 13:46:15 +0000

    test: add query parameter initialization unit tests
    - Test release query parameter (rhel8, rhel9, rhel10, invalid, missing)
    - Test architecture query parameter (aarch64, invalid, missing)
    - Test target query parameter (iso, qcow2, invalid, missing)
    - Test combined query parameters work together
    - Remove legacy ImageOutput integration tests now covered by unit tests

---
+++ src/Components/CreateImageWizard/tests/mocks/fixtures.ts
@@ -0,0 +1,40 @@
+import { Architectures } from '@/store/imageBuilderApi';
+
+export const mockArchitectures: Record<string, Architectures> = {
+  'rhel-8': [
+    {
+      arch: 'x86_64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+    {
+      arch: 'aarch64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+  ],
+  'rhel-9': [
+    {
+      arch: 'x86_64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+    {
+      arch: 'aarch64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+  ],
+  'rhel-10': [
+    {
+      arch: 'x86_64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+    {
+      arch: 'aarch64',
+      image_types: ['guest-image', 'image-installer'],
+      repositories: [],
+    },
+  ],
+};
