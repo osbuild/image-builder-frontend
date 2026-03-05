e247fcc924b2b4627dea525e0d94789b42b9ddd8
regexowl <ksimicko@redhat.com>
2026-03-05 15:18:29 +0100

    playwright: Set up basic Repeatable build tests
    This creates a new file for repeatable build tests and adds basic test structure.

---
+++ playwright/Customizations/RepeatableBuild.spec.ts
@@ -0,0 +1,114 @@
+import * as fsPromises from 'fs/promises';
+import * as path from 'path';
+
+import { expect } from '@playwright/test';
+import { v4 as uuidv4 } from 'uuid';
+
+import { test } from '../fixtures/customizations';
+import { exportedLocaleBP } from '../fixtures/data/exportBlueprintContents';
+import { isHosted } from '../helpers/helpers';
+import { ensureAuthenticated } from '../helpers/login';
+import {
+  fillInImageOutput,
+  ibFrame,
+  navigateToLandingPage,
+} from '../helpers/navHelpers';
+import {
+  createBlueprint,
+  deleteBlueprint,
+  exportBlueprint,
+  fillInDetails,
+  fillInImageOutputGuest,
+  importBlueprint,
+  registerLater,
+  verifyExportedBlueprint,
+} from '../helpers/wizardHelpers';
+
+test('Create a blueprint with Repeatable build customization', async ({
+  page,
+  cleanup,
+}) => {
+  test.skip(!isHosted(), 'Repeatable build is not available in the plugin');
+
+  const blueprintName = 'test-' + uuidv4();
+
+  // Delete the blueprint after the run fixture
+  cleanup.add(() => deleteBlueprint(page, blueprintName));
+
+  await ensureAuthenticated(page);
+
+  // Navigate to IB landing page and get the frame
+  await navigateToLandingPage(page);
+  const frame = ibFrame(page);
+
+  await test.step('Navigate to optional steps in Wizard', async () => {
+    await fillInImageOutput(frame);
+    await registerLater(frame);
+  });
+
+  await test.step('Select and fill the Repeatable build step', async () => {
+    await frame.getByRole('button', { name: 'Repeatable build' }).click();
+    await frame
+      .getByRole('radio', { name: /Enable repeatable build/i })
+      .click();
+    await frame.getByRole('button', { name: /clear date/i }).click();
+    await frame
+      .getByRole('textbox', { name: /date picker/i })
+      .fill('2026-01-01');
+    await frame.getByRole('button', { name: 'Review and finish' }).click();
+  });
+
+  await test.step('Fill the BP details', async () => {
+    await fillInDetails(frame, blueprintName);
+  });
+
+  await test.step('Create BP', async () => {
+    await createBlueprint(frame, blueprintName);
+  });
+
+  await test.step('Edit BP', async () => {
+    await frame.getByRole('button', { name: 'Edit blueprint' }).click();
+    await frame.getByRole('button', { name: 'Repeatable build' }).click(); // there is no revisit button specific to repeatable build
+    await expect(
+      frame.getByRole('textbox', { name: /date picker/i }),
+    ).toHaveValue('2026-01-01');
+    await frame.getByRole('button', { name: /today's date/i }).click();
+    await frame.getByRole('button', { name: 'Review and finish' }).click();
+    await frame
+      .getByRole('button', { name: 'Save changes to blueprint' })
+      .click();
+  });
+
+  let exportedBP = '';
+
+  await test.step('Export BP', async () => {
+    exportedBP = await exportBlueprint(page);
+    cleanup.add(async () => {
+      await fsPromises.rm(path.dirname(exportedBP), { recursive: true });
+    });
+  });
+
+  await test.step('Review exported BP', async (step) => {
+    step.skip(
+      isHosted(),
+      'Only verify the contents of the exported blueprint in cockpit',
+    );
+    verifyExportedBlueprint(exportedBP, exportedLocaleBP(blueprintName));
+  });
+
+  await test.step('Import BP', async () => {
+    await importBlueprint(frame, exportedBP);
+  });
+
+  await test.step('Review imported BP', async () => {
+    await fillInImageOutputGuest(frame);
+    await frame.getByRole('button', { name: 'Repeatable build' }).click();
+    await expect(
+      frame.getByRole('radio', { name: /Enable repeatable build/i }),
+    ).toBeEnabled();
+    await expect(
+      frame.getByRole('textbox', { name: /date picker/i }),
+    ).toHaveValue('2026-01-01');
+    await frame.getByRole('button', { name: 'Cancel' }).click();
+  });
+});
