6ae12fca1c336c21ce9cb1ffe9954d3241ddc15d
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-03-09 18:25:36 +0000

    store: delete emptyComposerCloudApi
    We were just creating this to discard it, we don't actually need it since
    we can just re-use one of the existing empty apis. This is a small tidy
    up.

--- api/config/composerCloudApi.ts
+++ api/config/composerCloudApi.ts
@@ -3,8 +3,8 @@ import type { ConfigFile } from '@rtk-query/codegen-openapi';
 const config: ConfigFile = {
   schemaFile:
     'https://raw.githubusercontent.com/osbuild/osbuild-composer/main/internal/cloudapi/v2/openapi.v2.yml',
-  apiFile: '../../src/store/cockpit/emptyComposerCloudApi.ts',
-  apiImport: 'emptyComposerCloudApi',
+  apiFile: '../../src/store/api/shared/emptyOnPremApi.ts',
+  apiImport: 'emptyOnPremApi',
   outputFile: '../../src/store/cockpit/composerCloudApi.ts',
   exportName: 'composerCloudApi',
   hooks: false,
