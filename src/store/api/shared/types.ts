7effea6cc15f2c7daa212ae69a3100bb3508ce43
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-03-13 12:41:47 +0000

    store: standardise the on-prem error types
    Add a toErrorString helper and standardise all the error handling
    for the on-prem apis for consistency.

--- src/store/api/shared/types.ts
+++ src/store/api/shared/types.ts
@@ -1,4 +1,17 @@
+import { BaseQueryFn } from '@reduxjs/toolkit/query';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Params = Record<string, any>;
 export type Method = 'GET' | 'DELETE' | 'POST' | 'PUT' | 'PATCH'; // We can add more if we need
 export type Headers = { [name: string]: string };
+
+// The request options type
+export type OnPremBaseQueryArgs = {
+  url: string;
+  method?: Method;
+  body?: unknown;
+  params?: Params;
+  headers?: Headers;
+};
+// The base query function type (matches what baseQuery() returns)
+export type OnPremBaseQuery = BaseQueryFn<OnPremBaseQueryArgs>;
