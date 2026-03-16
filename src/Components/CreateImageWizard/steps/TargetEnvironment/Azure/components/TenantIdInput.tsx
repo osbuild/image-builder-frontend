bd1795452024d06130a0b0c1147a294e6c2fa15e
regexowl <ksimicko@redhat.com>
2026-03-16 16:54:26 +0100

    Wizard: Azure - split index into components
    ssia

---
+++ src/Components/CreateImageWizard/steps/TargetEnvironment/Azure/components/TenantIdInput.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { FormGroup } from '@patternfly/react-core';
+
+import { ValidatedInput } from '@/Components/CreateImageWizard/ValidatedInput';
+import { isAzureTenantGUIDValid } from '@/Components/CreateImageWizard/validators';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { changeAzureTenantId, selectAzureTenantId } from '@/store/wizardSlice';
+
+const TenantIdInput = () => {
+  const dispatch = useAppDispatch();
+  const tenantId = useAppSelector(selectAzureTenantId);
+
+  return (
+    <FormGroup label='Azure tenant GUID' isRequired>
+      <ValidatedInput
+        aria-label='Azure tenant GUID'
+        value={tenantId || ''}
+        validator={isAzureTenantGUIDValid}
+        onChange={(_event, value) => dispatch(changeAzureTenantId(value))}
+        helperText={
+          !tenantId ? 'Tenant ID is required' : 'Please enter a valid tenant ID'
+        }
+        handleClear={() => dispatch(changeAzureTenantId(''))}
+      />
+    </FormGroup>
+  );
+};
+
+export default TenantIdInput;
