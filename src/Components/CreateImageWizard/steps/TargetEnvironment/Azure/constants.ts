c86f35675cfab65680e0313ba0f1dfc2ca3cdef2
regexowl <ksimicko@redhat.com>
2026-03-19 13:02:05 +0100

    Wizard: Move constants to localised file
    This moves the constant from a shared file to a localised one.

---
+++ src/Components/CreateImageWizard/steps/TargetEnvironment/Azure/constants.ts
@@ -0,0 +1,7 @@
+export const AZURE_AUTH_URL =
+  'https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow';
+
+export const HYPER_V_GENERATIONS = [
+  { value: 'V1', label: 'Hyper-V generation 1 (BIOS)' },
+  { value: 'V2', label: 'Hyper-V generation 2 (UEFI)' },
+];
