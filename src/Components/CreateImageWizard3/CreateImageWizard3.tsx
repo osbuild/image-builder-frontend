bc85f245467b7e8a1aeb37b162c0c900aed273d4
regexowl <ksimicko@redhat.com>
2026-03-27 14:28:42 +0100

    store: Clear the store when the Wizard is closed
    This clears the wizard state and resets the modal store when the Wizard is closed.

--- src/Components/CreateImageWizard3/CreateImageWizard3.tsx
+++ src/Components/CreateImageWizard3/CreateImageWizard3.tsx
@@ -188,6 +188,7 @@ export const CreateImageWizard3 = () => {
 
   const handleClose = () => {
     dispatch(closeWizardModal());
+    dispatch(initializeWizard());
 
     if (
       searchParams.has('release') ||
