6fd34b5413ac5a2b07c349a973d6b249d66997bd
regexowl <ksimicko@redhat.com>
2026-03-30 15:04:00 +0200

    Wizard: Remove Selected/Available states
    This removes the Selected/Available toggle and with it the logic for rendering the relevant states.
    
    The packages table should now render only selected items. Both individual packages and package groups should be visible next to each other.

--- src/Components/CreateImageWizard/steps/Packages/components/PackagesTable.tsx
+++ src/Components/CreateImageWizard/steps/Packages/components/PackagesTable.tsx
@@ -1,10 +1,4 @@
-import React, {
-  Dispatch,
-  ReactElement,
-  SetStateAction,
-  useMemo,
-  useState,
-} from 'react';
+import React, { ReactElement, useMemo, useState } from 'react';
 
 import {
   Button,
@@ -30,7 +24,6 @@ import { useDispatch } from 'react-redux';
 
 import { Module } from '@/store/api/backend';
 import { ApiRepositoryCollectionResponseRead } from '@/store/api/contentSources';
-import { selectIsOnPremise } from '@/store/slices/env';
 import {
   addModule,
   addPackage,
@@ -43,18 +36,13 @@ import {
 } from '@/store/slices/wizard';
 
 import EmptySearch from './EmptySearch';
-import NoResultsFound from './NoResultsFound';
 import PackageInfoNotAvailablePopover from './PackageInfoNotAvailablePopover';
 import RetirementDate from './RetirementDate';
-import Searching from './Searching';
-import TooShort from './TooShort';
-import TryLookingUnderIncluded from './TryLookingUnderIncluded';
 
 import { useAppSelector } from '../../../../../store/hooks';
 import {
   GroupWithRepositoryInfo,
   IBPackageWithRepositoryInfo,
-  Repos,
 } from '../packagesTypes';
 import { getPackageUniqueKey } from '../packagesUtilities';
 
@@ -66,9 +54,6 @@ type PackagesTableProps = {
     value: IBPackageWithRepositoryInfo | undefined,
   ) => void;
   setActiveStream: (value: string) => void;
-  setCurrentlyRemovedPackages: Dispatch<
-    SetStateAction<IBPackageWithRepositoryInfo[]>
-  >;
   packages: IBPackageWithRepositoryInfo[];
   groups: GroupWithRepositoryInfo[];
   modules: Module[];
@@ -77,27 +62,10 @@ type PackagesTableProps = {
   setActiveSortIndex: (value: number) => void;
   activeSortDirection: 'asc' | 'desc';
   setActiveSortDirection: (value: 'asc' | 'desc') => void;
-  transformedPackages: IBPackageWithRepositoryInfo[];
   activeStream: string;
   perPage: number;
   page: number;
-  showGroups: boolean;
-  showPackages: boolean;
-  transformedGroups: GroupWithRepositoryInfo[];
-  toggleSelected: string;
-  isLoadingRecommendedPackages: boolean;
-  isLoadingDistroPackages: boolean;
-  isLoadingCustomPackages: boolean;
-  isLoadingRecommendedGroups: boolean;
-  isLoadingDistroGroups: boolean;
-  isLoadingCustomGroups: boolean;
-  isLoadingReposInTemplate: boolean;
-  isSuccessRecommendedPackages: boolean;
   isSelectingPackage: IBPackageWithRepositoryInfo | undefined;
-  debouncedSearchTerm: string;
-  activeTabKey: Repos;
-  setActiveTabKey: (value: Repos) => void;
-  template: string;
 };
 
 const PackagesTable = ({
@@ -106,7 +74,6 @@ const PackagesTable = ({
   setIsRepoModalOpen,
   setIsSelectingPackage,
   setActiveStream,
-  setCurrentlyRemovedPackages,
   packages,
   groups,
   modules,
@@ -115,30 +82,12 @@ const PackagesTable = ({
   setActiveSortIndex,
   activeSortDirection,
   setActiveSortDirection,
-  transformedPackages,
   activeStream,
   perPage,
   page,
-  showGroups,
-  showPackages,
-  transformedGroups,
-  toggleSelected,
-  isLoadingRecommendedPackages,
-  isLoadingDistroPackages,
-  isLoadingCustomPackages,
-  isLoadingRecommendedGroups,
-  isLoadingDistroGroups,
-  isLoadingCustomGroups,
-  isLoadingReposInTemplate,
-  isSuccessRecommendedPackages,
   isSelectingPackage,
-  debouncedSearchTerm,
-  activeTabKey,
-  setActiveTabKey,
-  template,
 }: PackagesTableProps) => {
   const dispatch = useDispatch();
-  const isOnPremise = useAppSelector(selectIsOnPremise);
   const recommendedRepositories = useAppSelector(selectRecommendedRepositories);
 
   const handleSelect = (
@@ -167,16 +116,12 @@ const PackagesTable = ({
             }),
           );
         }
-        setCurrentlyRemovedPackages((prev) =>
-          prev.filter((curr) => curr.name !== pkg.name),
-        );
       }
     } else {
       dispatch(removePackage(pkg.name));
       if (pkg.type === 'module' && pkg.module_name) {
         dispatch(removeModule(pkg.module_name));
       }
-      setCurrentlyRemovedPackages((last) => [...last, pkg]);
       if (
         isSuccessEpelRepo &&
         epelRepo &&
@@ -275,12 +220,12 @@ const PackagesTable = ({
     expandedGroups.includes(group);
 
   const sortedPackages = useMemo(() => {
-    if (transformedPackages.length < 1 || !Array.isArray(transformedPackages)) {
+    if (packages.length < 1 || !Array.isArray(packages)) {
       return [];
     }
 
     return orderBy(
-      transformedPackages,
+      packages,
       [
         // Active stream packages first (if activeStream is set)
         (pkg) => (activeStream && pkg.stream === activeStream ? 0 : 1),
@@ -306,7 +251,15 @@ const PackagesTable = ({
       ],
       ['asc', 'asc', 'desc', 'asc', 'asc', 'asc'],
     );
-  }, [transformedPackages, activeStream]);
+  }, [packages, activeStream]);
+
+  const sortedGroups = useMemo(() => {
+    if (groups.length < 1 || !Array.isArray(groups)) {
+      return [];
+    }
+
+    return orderBy(groups, ['name'], ['asc']);
+  }, [groups]);
 
   const isPackageSelected = (pkg: IBPackageWithRepositoryInfo) => {
     let isSelected = false;
@@ -369,242 +322,180 @@ const PackagesTable = ({
   const composePkgTable = () => {
     let rows: ReactElement[] = [];
 
-    if (showGroups) {
-      rows = rows.concat(
-        transformedGroups
-          .slice(computeStart(), computeEnd())
-          .map((grp, rowIndex) => (
-            <Tbody
-              key={`${grp.name}-${grp.repository || 'default'}`}
-              isExpanded={isGroupExpanded(grp.name)}
-            >
-              <Tr data-testid='package-row'>
-                <Td
-                  expand={{
-                    rowIndex: rowIndex,
-                    isExpanded: isGroupExpanded(grp.name),
-                    onToggle: () =>
-                      setGroupsExpanded(grp.name, !isGroupExpanded(grp.name)),
-                    expandId: `${grp.name}-expandable`,
-                  }}
-                />
-                <Td
-                  select={{
-                    isSelected: groups.some((g) => g.name === grp.name),
-                    rowIndex: rowIndex,
-                    onSelect: (event, isSelecting) =>
-                      handleGroupSelect(grp, rowIndex, isSelecting),
-                  }}
-                />
-                <Td>
-                  @{grp.name}{' '}
-                  <Popover
-                    minWidth='25rem'
-                    headerContent='Included packages'
-                    bodyContent={
-                      <div
-                        style={
-                          grp.package_list?.length
-                            ? { height: '40em', overflow: 'scroll' }
-                            : {}
-                        }
-                      >
-                        {grp.package_list?.length ? (
-                          <Table
-                            variant='compact'
-                            data-testid='group-included-packages-table'
-                          >
-                            <Tbody>
-                              {grp.package_list.map((pkg: string) => (
-                                <Tr key={`details-${pkg}`}>
-                                  <Td>{pkg}</Td>
-                                </Tr>
-                              ))}
-                            </Tbody>
-                          </Table>
-                        ) : (
-                          <Content>This group has no packages</Content>
-                        )}
-                      </div>
+    rows = rows.concat(
+      sortedGroups.slice(computeStart(), computeEnd()).map((grp, rowIndex) => (
+        <Tbody
+          key={`${grp.name}-${grp.repository || 'default'}`}
+          isExpanded={isGroupExpanded(grp.name)}
+        >
+          <Tr data-testid='package-row'>
+            <Td
+              expand={{
+                rowIndex: rowIndex,
+                isExpanded: isGroupExpanded(grp.name),
+                onToggle: () =>
+                  setGroupsExpanded(grp.name, !isGroupExpanded(grp.name)),
+                expandId: `${grp.name}-expandable`,
+              }}
+            />
+            <Td
+              select={{
+                isSelected: groups.some((g) => g.name === grp.name),
+                rowIndex: rowIndex,
+                onSelect: (event, isSelecting) =>
+                  handleGroupSelect(grp, rowIndex, isSelecting),
+              }}
+            />
+            <Td>
+              @{grp.name}{' '}
+              <Popover
+                minWidth='25rem'
+                headerContent='Included packages'
+                bodyContent={
+                  <div
+                    style={
+                      grp.package_list?.length
+                        ? { height: '40em', overflow: 'scroll' }
+                        : {}
                     }
                   >
-                    <Button
-                      icon={<HelpIcon />}
-                      variant='plain'
-                      aria-label='About included packages'
-                      isInline
-                      size='sm'
-                      hasNoPadding
-                    />
-                  </Popover>
-                </Td>
-                <Td>N/A</Td>
-                <Td>N/A</Td>
-              </Tr>
-              <Tr isExpanded={isGroupExpanded(grp.name)}>
-                <Td colSpan={5}>
-                  <ExpandableRowContent>
-                    {
-                      <DescriptionList>
-                        <DescriptionListGroup>
-                          <DescriptionListTerm>
-                            Description
-                            {toggleSelected === 'toggle-selected' && (
-                              <PackageInfoNotAvailablePopover />
-                            )}
-                          </DescriptionListTerm>
-                          <DescriptionListDescription>
-                            {grp.description
-                              ? grp.description
-                              : 'Not available'}
-                          </DescriptionListDescription>
-                        </DescriptionListGroup>
-                      </DescriptionList>
-                    }
-                  </ExpandableRowContent>
-                </Td>
-              </Tr>
-            </Tbody>
-          )),
-      );
-    }
+                    {grp.package_list?.length ? (
+                      <Table
+                        variant='compact'
+                        data-testid='group-included-packages-table'
+                      >
+                        <Tbody>
+                          {grp.package_list.map((pkg: string) => (
+                            <Tr key={`details-${pkg}`}>
+                              <Td>{pkg}</Td>
+                            </Tr>
+                          ))}
+                        </Tbody>
+                      </Table>
+                    ) : (
+                      <Content>This group has no packages</Content>
+                    )}
+                  </div>
+                }
+              >
+                <Button
+                  icon={<HelpIcon />}
+                  variant='plain'
+                  aria-label='About included packages'
+                  isInline
+                  size='sm'
+                  hasNoPadding
+                />
+              </Popover>
+            </Td>
+            <Td>N/A</Td>
+            <Td>N/A</Td>
+          </Tr>
+          <Tr isExpanded={isGroupExpanded(grp.name)}>
+            <Td colSpan={5}>
+              <ExpandableRowContent>
+                {
+                  <DescriptionList>
+                    <DescriptionListGroup>
+                      <DescriptionListTerm>
+                        Description
+                        <PackageInfoNotAvailablePopover />
+                      </DescriptionListTerm>
+                      <DescriptionListDescription>
+                        {grp.description ? grp.description : 'Not available'}
+                      </DescriptionListDescription>
+                    </DescriptionListGroup>
+                  </DescriptionList>
+                }
+              </ExpandableRowContent>
+            </Td>
+          </Tr>
+        </Tbody>
+      )),
+    );
 
-    if (showPackages) {
-      rows = rows.concat(
-        sortedPackages
-          .slice(computeStart(), computeEnd())
-          .map((pkg, rowIndex) => (
-            <Tbody
-              key={`${pkg.name}-${pkg.stream || 'default'}-${pkg.module_name || pkg.name}`}
-              isExpanded={isPkgExpanded(pkg)}
-            >
-              <Tr data-testid='package-row'>
-                <Td
-                  expand={{
-                    rowIndex: rowIndex,
-                    isExpanded: isPkgExpanded(pkg),
-                    onToggle: () => setPkgExpanded(pkg, !isPkgExpanded(pkg)),
-                    expandId: `${pkg.name}-expandable`,
-                  }}
-                />
-                <Td
-                  select={{
-                    isSelected: isPackageSelected(pkg),
-                    rowIndex: rowIndex,
-                    onSelect: (event, isSelecting) =>
-                      handleSelect(pkg, rowIndex, isSelecting),
-                    isDisabled: isSelectDisabled(pkg),
-                  }}
-                  title={
-                    isSelectDisabled(pkg)
-                      ? 'Disabled due to the package(s) you selected. You cannot select packages from different application stream versions.'
-                      : ''
+    rows = rows.concat(
+      sortedPackages
+        .slice(computeStart(), computeEnd())
+        .map((pkg, rowIndex) => (
+          <Tbody
+            key={`${pkg.name}-${pkg.stream || 'default'}-${pkg.module_name || pkg.name}`}
+            isExpanded={isPkgExpanded(pkg)}
+          >
+            <Tr data-testid='package-row'>
+              <Td
+                expand={{
+                  rowIndex: rowIndex,
+                  isExpanded: isPkgExpanded(pkg),
+                  onToggle: () => setPkgExpanded(pkg, !isPkgExpanded(pkg)),
+                  expandId: `${pkg.name}-expandable`,
+                }}
+              />
+              <Td
+                select={{
+                  isSelected: isPackageSelected(pkg),
+                  rowIndex: rowIndex,
+                  onSelect: (event, isSelecting) =>
+                    handleSelect(pkg, rowIndex, isSelecting),
+                  isDisabled: isSelectDisabled(pkg),
+                }}
+                title={
+                  isSelectDisabled(pkg)
+                    ? 'Disabled due to the package(s) you selected. You cannot select packages from different application stream versions.'
+                    : ''
+                }
+              />
+              <Td>{pkg.name}</Td>
+              <Td>{pkg.stream ? pkg.stream : 'N/A'}</Td>
+              <Td>
+                <RetirementDate date={pkg.end_date} />
+              </Td>
+            </Tr>
+            <Tr isExpanded={isPkgExpanded(pkg)}>
+              <Td colSpan={5}>
+                <ExpandableRowContent>
+                  {
+                    <DescriptionList>
+                      <DescriptionListGroup>
+                        <DescriptionListTerm>
+                          Description
+                          <PackageInfoNotAvailablePopover />
+                        </DescriptionListTerm>
+                        <DescriptionListDescription>
+                          {pkg.summary ? pkg.summary : 'Not available'}
+                        </DescriptionListDescription>
+                      </DescriptionListGroup>
+                    </DescriptionList>
                   }
-                />
-                <Td>{pkg.name}</Td>
-                <Td>{pkg.stream ? pkg.stream : 'N/A'}</Td>
-                <Td>
-                  <RetirementDate date={pkg.end_date} />
-                </Td>
-              </Tr>
-              <Tr isExpanded={isPkgExpanded(pkg)}>
-                <Td colSpan={5}>
-                  <ExpandableRowContent>
-                    {
-                      <DescriptionList>
-                        <DescriptionListGroup>
-                          <DescriptionListTerm>
-                            Description
-                            {toggleSelected === 'toggle-selected' && (
-                              <PackageInfoNotAvailablePopover />
-                            )}
-                          </DescriptionListTerm>
-                          <DescriptionListDescription>
-                            {pkg.summary ? pkg.summary : 'Not available'}
-                          </DescriptionListDescription>
-                        </DescriptionListGroup>
-                      </DescriptionList>
-                    }
-                  </ExpandableRowContent>
-                </Td>
-              </Tr>
-            </Tbody>
-          )),
-      );
-    }
+                </ExpandableRowContent>
+              </Td>
+            </Tr>
+          </Tbody>
+        )),
+    );
     return rows;
   };
 
   const bodyContent = useMemo(() => {
-    switch (true) {
-      case debouncedSearchTerm.length === 1 &&
-        !debouncedSearchTerm.startsWith('@'):
-        return <TooShort />;
-      case (toggleSelected === 'toggle-selected' &&
-        packages.length === 0 &&
-        groups.length === 0) ||
-        (!debouncedSearchTerm && toggleSelected === 'toggle-available'):
-        return <EmptySearch toggleSelected={toggleSelected} />;
-      case (debouncedSearchTerm &&
-        (isLoadingRecommendedPackages || isLoadingRecommendedGroups) &&
-        activeTabKey === Repos.OTHER) ||
-        (debouncedSearchTerm &&
-          (isLoadingDistroPackages ||
-            isLoadingCustomPackages ||
-            isLoadingDistroGroups ||
-            isLoadingReposInTemplate ||
-            isLoadingCustomGroups) &&
-          activeTabKey === Repos.INCLUDED):
-        return <Searching activeTabKey={activeTabKey} />;
-      case debouncedSearchTerm &&
-        transformedPackages.length === 0 &&
-        transformedGroups.length === 0 &&
-        toggleSelected === 'toggle-available':
-        return (
-          <NoResultsFound
-            isOnPremise={isOnPremise}
-            activeTabKey={activeTabKey}
-            setActiveTabKey={setActiveTabKey}
-          />
-        );
-      case debouncedSearchTerm &&
-        toggleSelected === 'toggle-selected' &&
-        activeTabKey === Repos.OTHER &&
-        packages.length > 0 &&
-        groups.length > 0:
-        return <TryLookingUnderIncluded setActiveTabKey={setActiveTabKey} />;
-      default:
-        return composePkgTable();
+    if (packages.length === 0 && groups.length === 0) {
+      return <EmptySearch />;
     }
+    return composePkgTable();
     // Would need significant rewrite to fix this
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     page,
     perPage,
-    debouncedSearchTerm,
-    isLoadingCustomPackages,
-    isLoadingDistroPackages,
-    isLoadingRecommendedPackages,
-    isSuccessRecommendedPackages,
-    isLoadingDistroGroups,
-    isLoadingCustomGroups,
-    isLoadingRecommendedGroups,
     packages.length,
     groups.length,
-    toggleSelected,
-    activeTabKey,
-    transformedPackages,
-    transformedGroups,
     isSelectingPackage,
     recommendedRepositories,
-    transformedPackages.length,
-    transformedGroups.length,
     expandedPkgs,
     expandedGroups,
     sortedPackages,
+    sortedGroups,
     activeSortDirection,
     activeSortIndex,
-    template,
   ]);
 
   return (
