0d793b7e29e1893380ef6f598b020cada36c25d1
regexowl <ksimicko@redhat.com>
2026-04-07 15:48:30 +0200

    playwright: Update OpenSCAP tests
    The OpenSCAP definitions were updated upstream, this reflects the change.

--- playwright/Customizations/OpenSCAP.spec.ts
+++ playwright/Customizations/OpenSCAP.spec.ts
@@ -111,7 +111,7 @@ test('Create a blueprint with OpenSCAP customization', async ({
       frame.getByRole('gridcell', { name: 'systemd-journal-remote' }),
     ).toBeVisible();
     await frame.getByRole('button', { name: 'Systemd services' }).click();
-    await expect(frame.getByText('10 Added by OpenSCAP')).toBeVisible();
+    await expect(frame.getByText('11 Added by OpenSCAP')).toBeVisible();
     await frame.getByPlaceholder('Add masked service').fill('nftables');
     await frame.getByPlaceholder('Add masked service').press('Enter');
     await expect(
@@ -179,7 +179,7 @@ test('Create a blueprint with OpenSCAP customization', async ({
     ).toBeVisible();
     await expect(frame.getByRole('gridcell', { name: 'sudo' })).toBeVisible();
     await frame.getByRole('button', { name: 'Systemd services' }).click();
-    await expect(frame.getByText('11 Added by OpenSCAP')).toBeVisible();
+    await expect(frame.getByText('12 Added by OpenSCAP')).toBeVisible();
     await frame.getByPlaceholder('Add masked service').fill('nftables');
     await frame.getByPlaceholder('Add masked service').press('Enter');
     await expect(
