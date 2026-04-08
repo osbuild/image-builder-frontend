193e20897e34d1842a97ca407adbc94e91d9ef5c
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-04-08 11:24:35 +0100

    Wizard/Review: update review step header

--- src/Components/CreateImageWizard/steps/Review/index.tsx
+++ src/Components/CreateImageWizard/steps/Review/index.tsx
@@ -6,20 +6,32 @@ import { useAppSelector } from '@/store/hooks';
 import {
   selectBlueprintDescription,
   selectBlueprintName,
-} from '@/store/slices/wizard';
+} from '@/store/slices';
+import { useFlag } from '@/Utilities/useGetEnvironment';
 
 import Review from './components/ReviewStep';
 
-const ReviewStep = () => {
+const FormHeader = () => {
+  const isWizardRevampEnabled = useFlag('image-builder.wizard-revamp.enabled');
   const blueprintName = useAppSelector(selectBlueprintName);
   const blueprintDescription = useAppSelector(selectBlueprintDescription);
 
+  if (isWizardRevampEnabled) return null;
+
   return (
-    <Form>
+    <>
       <Title headingLevel='h1' size='xl'>
         Review {blueprintName} blueprint
       </Title>
       {blueprintDescription && <Content>{blueprintDescription}</Content>}
+    </>
+  );
+};
+
+const ReviewStep = () => {
+  return (
+    <Form>
+      <FormHeader />
       <Review />
     </Form>
   );
