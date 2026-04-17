f1f4ca8826107cc4939ebced73652271e8f2f673
regexowl <ksimicko@redhat.com>
2026-04-17 15:01:00 +0200

    Wizard: Gate text formatting

--- src/Components/CreateImageWizard/steps/UserGroups/index.tsx
+++ src/Components/CreateImageWizard/steps/UserGroups/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Content, Form, Title } from '@patternfly/react-core';
 
+import { useFlag } from '@/Utilities/useGetEnvironment';
+
 import GroupInfo from './components/GroupInfo';
 
 import { CustomizationLabels } from '../../../sharedComponents/CustomizationLabels';
@@ -11,19 +13,23 @@ type UsersStepProps = {
 };
 
 const UserGroupsStep = ({ attemptedNext }: UsersStepProps) => {
+  const isWizardRevampEnabled = useFlag('image-builder.wizard-revamp.enabled');
+
+  const Wrapper = isWizardRevampEnabled ? React.Fragment : Form;
+
   return (
-    <Form>
+    <Wrapper>
       <CustomizationLabels customization='users' />
       <Title headingLevel='h2' size='lg'>
         Groups
       </Title>
-      <Content>
+      <Content component={isWizardRevampEnabled ? 'small' : 'p'}>
         Define groups before assigning users to them. Each group will be created
         on systems using this image. Each group will automatically be assigned
         an ID number.
       </Content>
       <GroupInfo attemptedNext={attemptedNext} />
-    </Form>
+    </Wrapper>
   );
 };
 
--- src/Components/CreateImageWizard/steps/Users/index.tsx
+++ src/Components/CreateImageWizard/steps/Users/index.tsx
@@ -5,6 +5,7 @@ import { Content, Form, Title } from '@patternfly/react-core';
 import { CustomizationLabels } from '@/Components/sharedComponents/CustomizationLabels';
 import { useAppSelector } from '@/store/hooks';
 import { selectBlueprintMode } from '@/store/slices/wizard';
+import { useFlag } from '@/Utilities/useGetEnvironment';
 
 import UserInfo from './components/UserInfo';
 
@@ -14,25 +15,28 @@ type UsersStepProps = {
 
 const UsersStep = ({ attemptedNext }: UsersStepProps) => {
   const blueprintMode = useAppSelector(selectBlueprintMode);
+  const isWizardRevampEnabled = useFlag('image-builder.wizard-revamp.enabled');
+
+  const Wrapper = isWizardRevampEnabled ? React.Fragment : Form;
 
   return (
-    <Form>
+    <Wrapper>
       <CustomizationLabels customization='users' />
       <Title headingLevel='h2' size='lg'>
         Users
       </Title>
-      <Content>
+      <Content component={isWizardRevampEnabled ? 'small' : 'p'}>
         Create user accounts for systems that will use this image. Duplicate
         usernames are not allowed.
       </Content>
       {blueprintMode === 'image' && (
-        <Content>
+        <Content component={isWizardRevampEnabled ? 'small' : 'p'}>
           You must create a user during the image build process to be able to
           log in.
         </Content>
       )}
       <UserInfo attemptedNext={attemptedNext} />
-    </Form>
+    </Wrapper>
   );
 };
 
