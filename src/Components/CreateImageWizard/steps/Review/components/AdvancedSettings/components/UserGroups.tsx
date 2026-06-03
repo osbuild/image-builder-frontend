128e6afb2c64baaf24fbd5fbf1f9b6a0007e6875
regexowl <ksimicko@redhat.com>
2026-06-03 15:25:33 +0200

    Wizard: Add user groups to the review step
    ssia

---
+++ src/Components/CreateImageWizard/steps/Review/components/AdvancedSettings/components/UserGroups.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Flex } from '@patternfly/react-core';
+
+import { useAppSelector } from '@/store/hooks';
+import { selectUserGroups } from '@/store/slices/wizard';
+
+import { FlexColumn, ReviewGroup } from '../../shared';
+import { Hideable } from '../../types';
+
+export const UserGroups = ({ shouldHide }: Hideable) => {
+  const userGroups = useAppSelector(selectUserGroups);
+
+  if (shouldHide || userGroups.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <ReviewGroup heading='User groups' />
+      <Flex flexWrap={{ default: 'nowrap' }}>
+        <FlexColumn
+          heading='Name'
+          labelKey='user-group-name-review'
+          items={userGroups.map(({ name }) => name)}
+        />
+        <FlexColumn
+          heading='Group ID'
+          labelKey='user-group-gid-review'
+          items={userGroups.map(({ gid }) => gid ?? 'None')}
+        />
+      </Flex>
+    </>
+  );
+};
