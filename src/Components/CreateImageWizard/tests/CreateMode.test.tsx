acacde217010ff71f936c54cd0f3f7899ea8c59b
regexowl <ksimicko@redhat.com>
2026-06-26 10:53:59 +0200

    vitest: Migrate CreatImageWizard.test.tsx to co-located tests
    This migrates the old test file to the new co-located unit tests.

---
+++ src/Components/CreateImageWizard/tests/CreateMode.test.tsx
@@ -0,0 +1,100 @@
+import { screen, waitFor, within } from '@testing-library/react';
+import { vi } from 'vitest';
+
+import { composeHandlers, createArchitecturesHandler } from '@/test/testUtils';
+
+import { renderCreateMode, testCheckbox } from './helpers';
+import {
+  createDefaultFetchHandler,
+  fetchMock,
+  mockArchitectures,
+} from './mocks';
+
+describe('Create Image Wizard', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    fetchMock.enableMocks();
+    fetchMock.mockResponse(createDefaultFetchHandler());
+  });
+
+  afterEach(() => {
+    fetchMock.disableMocks();
+  });
+
+  test('renders component', async () => {
+    await renderCreateMode();
+
+    // check heading
+    await screen.findByRole('heading', { name: /Build an image/ });
+
+    // check navigation
+    await screen.findByRole('button', { name: 'Base settings' });
+    await screen.findByRole('button', { name: 'Repositories and packages' });
+    await screen.findByRole('button', { name: 'Advanced settings' });
+    await screen.findByRole('button', { name: 'Review' });
+  });
+
+  test('should only enable first navigation item in create mode', async () => {
+    await renderCreateMode();
+
+    const navigation = await screen.findByRole('navigation', {
+      name: /wizard steps/i,
+    });
+    const baseNavItem = within(navigation).getByRole('button', {
+      name: /base settings/i,
+    });
+    const contentNavItem = within(navigation).getByRole('button', {
+      name: /repositories and packages/i,
+    });
+    const advancedNavItem = within(navigation).getByRole('button', {
+      name: /advanced settings/i,
+    });
+    const reviewNavItem = within(navigation).getByRole('button', {
+      name: /review/i,
+    });
+
+    expect(baseNavItem).toBeEnabled();
+    expect(contentNavItem).toBeDisabled();
+    expect(advancedNavItem).toBeDisabled();
+    expect(reviewNavItem).toBeDisabled();
+  });
+});
+
+describe('Keyboard accessibility', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    fetchMock.enableMocks();
+    fetchMock.mockResponse(
+      composeHandlers(
+        createArchitecturesHandler({ architectures: mockArchitectures }),
+        createDefaultFetchHandler(),
+      ),
+    );
+  });
+
+  afterEach(() => {
+    fetchMock.disableMocks();
+  });
+
+  test('target environment checkboxes are keyboard selectable', async () => {
+    await renderCreateMode();
+
+    await waitFor(() =>
+      expect(
+        screen.queryByRole('heading', {
+          name: /loading target environments/i,
+        }),
+      ).not.toBeInTheDocument(),
+    );
+
+    await testCheckbox(
+      await screen.findByRole('checkbox', { name: /Amazon Web Services/i }),
+    );
+    await testCheckbox(
+      await screen.findByRole('checkbox', { name: /Google Cloud/i }),
+    );
+    await testCheckbox(
+      await screen.findByRole('checkbox', { name: /Microsoft Azure/i }),
+    );
+  });
+});
--- src/test/cockpit-tests.sh
+++ src/test/cockpit-tests.sh
@@ -5,5 +5,3 @@ export TZ=UTC IS_ON_PREMISE=true
 vitest run -t 'Images Table render ImagesTable' \
        src/test/Components/ImagesTable/ImagesTable.test.tsx
 
-vitest run -t 'Create Image Wizard renders component' \
-       src/test/Components/CreateImageWizard/CreateImageWizard.test.tsx
