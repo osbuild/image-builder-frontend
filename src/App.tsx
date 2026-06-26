fbe1002c785c1f20c21f6330be1081cb78e0304d
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-06-26 08:27:50 +0100

    app: resolve org ID at initialization
    Fetch the org ID from auth.getUser() once at app mount and store it
    in Redux. This value never changes during a session, so resolving it
    early means downstream consumers can read it synchronously from state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
 import React, { useEffect } from 'react';
 
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import { useAddNotification } from '@redhat-cloud-services/frontend-components-notifications/hooks';
 import NotificationsProvider from '@redhat-cloud-services/frontend-components-notifications/NotificationsProvider';
 import '@patternfly/patternfly/patternfly-addons.css';
 
 import { Router } from './Router';
+import { useAppDispatch } from './store/hooks';
+import { changeOrgId } from './store/slices/wizard';
 
-const App = () => {
-  const { hideGlobalFilter, updateDocumentTitle } = useChrome();
+const AppContent = () => {
+  const { auth, hideGlobalFilter, updateDocumentTitle } = useChrome();
+  const dispatch = useAppDispatch();
+  const addNotification = useAddNotification();
+  const getUser = auth.getUser;
 
   useEffect(() => {
     updateDocumentTitle('Images');
     hideGlobalFilter(true);
   }, [hideGlobalFilter, updateDocumentTitle]);
 
+  useEffect(() => {
+    // the org id never changes, let's just get
+    // this on app init rather than every time
+    // the user creates a blueprint
+    const resolveOrgId = async () => {
+      const userData = await getUser();
+      const orgId = userData?.identity.internal?.org_id;
+      if (orgId) {
+        dispatch(changeOrgId(orgId));
+        return;
+      }
+      throw new Error('Unable to determine organization ID');
+    };
+
+    resolveOrgId().catch(() => {
+      addNotification({
+        variant: 'danger',
+        title:
+          'Unable to determine your organization ID. Some features may not work correctly.',
+      });
+    });
+  }, [getUser, dispatch, addNotification]);
+
+  return <Router />;
+};
+
+const App = () => {
   return (
     <React.Fragment>
       <NotificationsProvider>
-        <Router />
+        <AppContent />
       </NotificationsProvider>
     </React.Fragment>
   );
