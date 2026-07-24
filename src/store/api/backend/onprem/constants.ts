b4bceebd94c23f983d3e39446a8e5c467d6496a4
Gianluca Zuccarelli <gzuccare@redhat.com>
2026-07-24 16:35:33 +0100

    api: update on-prem known images for RHEL 10.3
    Replace the rhel-bootc reference with the rhel-kvm guest image and add
    the rhel-aws image as a second known image entry.

--- src/store/api/backend/onprem/constants.ts
+++ src/store/api/backend/onprem/constants.ts
@@ -4,10 +4,16 @@ export type KnownImage = Omit<BootcDistributionItem, 'arch'>;
 
 export const KNOWN_IMAGES: KnownImage[] = [
   {
-    reference: 'registry.redhat.io/rhel10/rhel-bootc:latest',
+    reference: 'registry.redhat.io/rhel10/rhel-kvm:latest',
     distro: 'rhel-10.3',
     name: 'Red Hat Enterprise Linux (RHEL) 10.3',
     type: 'guest-image',
+  },
+  {
+    reference: 'registry.redhat.io/rhel10/rhel-aws:latest',
+    distro: 'rhel-10.3',
+    name: 'Red Hat Enterprise Linux (RHEL) 10.3',
+    type: 'aws',
   },
 ];
 
