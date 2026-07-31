ddd7b73691fc938b189c27f5f7971d29d697f6c5
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2026-07-31 17:48:05 +0200

    src/store/onprem: parse upload config to generate cloud targets
    Rather than reading the toml worker config, read the upload-config and
    ensure AWS variables are defined.

--- src/store/api/backend/onprem/composerApi/helpers/getCloudConfigs.ts
+++ src/store/api/backend/onprem/composerApi/helpers/getCloudConfigs.ts
@@ -1,15 +1,23 @@
-import cockpit from 'cockpit';
-import TOML from 'smol-toml';
+import { getBlueprintsPath } from './getBlueprintsPath';
+import { safeReadJsonFile } from './safeReadJsonFile';
+
+import type { UploadConfigFile } from '../../../onprem';
 
 export const getCloudConfigs = async (): Promise<string[]> => {
-  try {
-    const worker_config = cockpit.file(
-      '/etc/osbuild-worker/osbuild-worker.toml',
-    );
-    const contents = await worker_config.read();
-    const parsed = TOML.parse(contents);
-    return Object.keys(parsed).filter((k) => k === 'aws');
-  } catch {
-    return [];
+  const stateDir = await getBlueprintsPath();
+  const uploadConfig = await safeReadJsonFile<UploadConfigFile>(
+    path.join(stateDir, 'upload-config.json'),
+  );
+
+  if (
+    uploadConfig &&
+    upload.config.aws &&
+    uploadConfig.aws.profile &&
+    uploadConfig.aws.credentials &&
+    uploadConfig.aws.region &&
+    uploadConfig.aws.bucket
+  ) {
+    return ['aws'];
   }
+  return [];
 };
