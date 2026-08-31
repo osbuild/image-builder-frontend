b70f1ccdecc07425fb0d1c6bdd94f445cbc81d5a
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-08-31 15:35:39 +0100

    store/wizard: add validateKernelArgs wrapper for TextInputGroup

--- src/store/slices/wizard/system/validators.ts
+++ src/store/slices/wizard/system/validators.ts
@@ -1,7 +1,11 @@
-import { hostnameSchema } from './schemas';
+import { hostnameSchema, kernelSchema } from './schemas';
 
-import { validateSchema } from '../validators';
+import { validateSchema, validateList } from '../validators';
 
 export const validateHostname = (hostname: string) => {
   return validateSchema(hostnameSchema, hostname);
+};
+
+export const validateKernelArgs = (items: string[]) => {
+  return validateList(kernelSchema.shape.append, items);
 };
