f3995f13177326548324055cad8b67ecbde3c1f5
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-08-31 17:13:43 +0100

    KernelArguments: use useSecuritySummary for OpenSCAP kernel args
    Replace manual useGetOscapCustomizationsQuery with useSecuritySummary
    hook which already parses the kernel append string into an array.
    
    Behaviour change: OpenSCAP-required kernel args are now always shown
    as required labels, even if they haven't been added to kernelAppend
    yet. Previously they only appeared once present in the store.

--- src/Components/CreateImageWizard/steps/Kernel/components/KernelArguments.tsx
+++ src/Components/CreateImageWizard/steps/Kernel/components/KernelArguments.tsx
@@ -5,13 +5,11 @@ import { FormGroup } from '@patternfly/react-core';
 import LabelInput from '@/Components/CreateImageWizard/LabelInput';
 import { useKernelValidation } from '@/Components/CreateImageWizard/utilities/useValidation';
 import { isKernelArgumentValid } from '@/Components/CreateImageWizard/validators';
-import { useGetOscapCustomizationsQuery } from '@/store/api/backend';
+import { useSecuritySummary } from '@/store/api/backend';
 import { useAppSelector } from '@/store/hooks';
 import {
   addKernelArg,
   removeKernelArg,
-  selectComplianceProfileID,
-  selectDistribution,
   selectKernel,
 } from '@/store/slices/wizard';
 
@@ -19,24 +17,7 @@ const KernelArguments = () => {
   const kernelAppend = useAppSelector(selectKernel).append;
 
   const stepValidation = useKernelValidation();
-
-  const release = useAppSelector(selectDistribution);
-  const complianceProfileID = useAppSelector(selectComplianceProfileID);
-
-  const { data: oscapProfileInfo } = useGetOscapCustomizationsQuery(
-    {
-      distribution: release,
-      // @ts-ignore if complianceProfileID is undefined the query is going to get skipped, so it's safe here to ignore the linter here
-      profile: complianceProfileID,
-    },
-    {
-      skip: !complianceProfileID,
-    },
-  );
-
-  const requiredByOpenSCAP = kernelAppend.filter((arg) =>
-    oscapProfileInfo?.kernel?.append?.split(' ').includes(arg),
-  );
+  const { kernel: oscapKernel } = useSecuritySummary();
 
   return (
     <FormGroup isRequired={false} label='Arguments'>
@@ -44,8 +25,8 @@ const KernelArguments = () => {
         ariaLabel='Add kernel argument'
         placeholder='Add kernel argument'
         validator={isKernelArgumentValid}
-        list={kernelAppend.filter((arg) => !requiredByOpenSCAP.includes(arg))}
-        requiredList={requiredByOpenSCAP}
+        list={kernelAppend.filter((arg) => !oscapKernel.append.includes(arg))}
+        requiredList={oscapKernel.append}
         item='Kernel argument'
         addAction={addKernelArg}
         removeAction={removeKernelArg}
