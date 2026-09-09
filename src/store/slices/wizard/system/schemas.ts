b17dec3a9f6161edfed6ea1177c30311fbca58cf
regexowl <ksimicko@redhat.com>
2026-09-09 16:51:12 +0200

    Wizard/Kernel: Allow space as valid character
    Spaces are valid characters in kernel arguments. This allows them and updates tests to reflect the change.

--- src/store/slices/wizard/system/schemas.ts
+++ src/store/slices/wizard/system/schemas.ts
@@ -31,7 +31,7 @@ export const kernelArgSchema = z
     // (#, \, ", ', ;) because they are valid in kernel arguments. The backend
     // is responsible for escaping these before writing them to the bootloader
     // config to prevent grub/bootloader injection.
-    /^[a-zA-Z0-9=\-_,."'/:#+;\\]*$/,
+    /^[a-zA-Z0-9=\-_,."'/:#+;\\ ]*$/,
     'Kernel argument contains invalid characters',
   );
 
