cd7f726c3ea9cbbc2c6b020326ce2067a91725d2
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-09-10 14:34:56 +0100

    store/wizard: add timezone schema

--- src/store/slices/wizard/system/types.ts
+++ src/store/slices/wizard/system/types.ts
@@ -1,17 +1,19 @@
 import z from 'zod';
 
-import { Locale, Timezone, User } from '@/store/api/backend';
+import { Locale, User } from '@/store/api/backend';
 
 import {
   firewallSchema,
   kernelSchema,
   servicesSchema,
   systemSchema,
+  timezoneSchema,
 } from './schemas';
 
 export type Kernel = z.infer<typeof kernelSchema>;
 export type Services = z.infer<typeof servicesSchema>;
 export type Firewall = z.infer<typeof firewallSchema>;
+export type Timezone = z.infer<typeof timezoneSchema>;
 
 export type UserWithAdditionalInfo = {
   [K in keyof User]-?: NonNullable<User[K]>;
@@ -62,7 +64,6 @@ export type UserGroup = {
 
 export type SystemSlice = z.infer<typeof systemSchema> & {
   locale: Locale;
-  timezone: Timezone;
   firstBoot: {
     script: string;
   };
